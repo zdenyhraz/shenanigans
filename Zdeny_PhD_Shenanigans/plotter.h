29ccf2bd526fb66f5a086b096859290a73ea872f
zdenyhraz <hrazdira.zdenek@gmail.com>
2019-12-19 22:54:52 +0100

    progress

--- Zdeny_PhD_Shenanigans/plotter.h
+++ Zdeny_PhD_Shenanigans/plotter.h
@@ -3,12 +3,14 @@
 
 struct AbstractPlot1D
 {
+public:
 	inline virtual void plot(const std::vector<double>& x, const std::vector<double>& y) = 0;
 	inline virtual void setAxisNames(std::string xlabel, std::string ylabel) = 0;
 };
 
 struct AbstractPlot2D
 {
+public:
 	inline virtual void plot(const std::vector<std::vector<double>>& z) = 0;
 	inline virtual void setAxisNames(std::string xlabel, std::string ylabel, std::string zlabel) = 0;
 };
