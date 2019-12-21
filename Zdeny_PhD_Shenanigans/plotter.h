3bf59c81812a14b476ad97d1d653877803a0459e
zdenyhraz <hrazdira.zdenek@gmail.com>
2019-12-21 02:45:18 +0100

    plots clear

--- Zdeny_PhD_Shenanigans/plotter.h
+++ Zdeny_PhD_Shenanigans/plotter.h
@@ -3,16 +3,16 @@
 
 struct AbstractPlot1D
 {
-public:
 	inline virtual void plot(const std::vector<double>& x, const std::vector<double>& y) = 0;
 	inline virtual void setAxisNames(std::string xlabel, std::string ylabel) = 0;
+	inline virtual void save(std::string path) = 0;
 };
 
 struct AbstractPlot2D
 {
-public:
 	inline virtual void plot(const std::vector<std::vector<double>>& z) = 0;
 	inline virtual void setAxisNames(std::string xlabel, std::string ylabel, std::string zlabel) = 0;
+	inline virtual void save(std::string path) = 0;
 };
 
 
