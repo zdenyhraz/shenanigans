f034cd11701b8c26fe85b8cca1edb63968bbcdde
zdenyhraz <hrazdira.zdenek@gmail.com>
2019-12-22 21:36:47 +0100

    opt add data qcp

--- Zdeny_PhD_Shenanigans/plotter.h
+++ Zdeny_PhD_Shenanigans/plotter.h
@@ -5,6 +5,9 @@ struct AbstractPlot1D
 {
 	inline virtual void plot(const std::vector<double>& x, const std::vector<double>& y) = 0;
 	inline virtual void plot(const std::vector<double>& x, const std::vector<double>& y1, const std::vector<double>& y2) = 0;
+	inline virtual void plot(const double x, const double y) = 0;
+	inline virtual void plot(const double x, const double y1, const double y2) = 0;
+	inline virtual void clear(bool second) = 0;
 	inline virtual void setAxisNames(std::string xlabel, std::string ylabel) = 0;
 	inline virtual void setAxisNames(std::string xlabel, std::string ylabel1, std::string ylabel2) = 0;
 	inline virtual void save(std::string path) = 0;
@@ -18,5 +21,3 @@ struct AbstractPlot2D
 	inline virtual void setSize(int nx, int ny) = 0;
 	inline virtual void setRange(double xmin, double xmax, double ymin, double ymax) = 0;
 };
-
-
