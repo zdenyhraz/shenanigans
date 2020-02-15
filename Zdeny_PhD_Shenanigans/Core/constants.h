f137f63c0f6a91a5bf8bd3566d8e843f64db9fee
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-02-15 19:21:46 +0100

    proc

--- Zdeny_PhD_Shenanigans/Core/constants.h
+++ Zdeny_PhD_Shenanigans/Core/constants.h
@@ -3,7 +3,7 @@
 namespace Constants
 {
 	constexpr double Pi = 3.1415926535897932384626433;
-	constexpr double TwoPi = 2 * Pi;
+	constexpr double TwoPi = 2. * Pi;
 	constexpr double HalfPi = 0.5 * Pi;
 	constexpr double E = 2.7182818284590452353602874;
 }
