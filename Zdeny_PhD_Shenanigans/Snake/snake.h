67eb2a1e3653081f3d1e2ffd9168e172a02f67d5
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-02-16 12:25:35 +0100

    snake

--- Zdeny_PhD_Shenanigans/Snake/snake.h
+++ Zdeny_PhD_Shenanigans/Snake/snake.h
@@ -5,18 +5,28 @@ using namespace cv;
 
 class Map;
 
-struct Coordinate
-{
-	Coordinate(unsigned x_, unsigned y_) :x(x_), y(y_) {}
-
-	unsigned x = 0;
-	unsigned y = 0;
-};
-
 class Snake
 {
 public:
 	Snake(Map& map);
+
+	struct Coordinate
+	{
+		Coordinate(int x_, int y_) :x(x_), y(y_) {}
+
+		int x = 0;
+		int y = 0;
+
+		bool operator==(const Coordinate& other)
+		{
+			return (x == other.x) && (y == other.y);
+		}
+
+		bool operator!=(const Coordinate& other)
+		{
+			return !(*this == other);
+		}
+	};
 
 	enum Direction
 	{
@@ -26,16 +36,19 @@ public:
 		RIGHT
 	};
 
+	void Tick(Coordinate food);
+
+	std::vector<Coordinate> GetBody();
+
+	bool GetGameOver();
+
 private:
 	std::vector<Coordinate> m_body;
 	Direction m_direction;
 	bool m_gameover;
-
-	void Grow();
+	Map& m_map;
 
 	bool CheckValidMove();
-
-	void Tick();
 
 	void Turn(Direction direction);
 
