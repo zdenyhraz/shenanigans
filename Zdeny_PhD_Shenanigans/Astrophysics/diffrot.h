1f6356731c7bc886446071634e6e7a267cea0bf2
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-02-19 22:36:37 +0100

    diffrot attempt 2

---
+++ Zdeny_PhD_Shenanigans/Astrophysics/diffrot.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "stdafx.h"
+#include "functionsAstro.h"
+
+DiffrotResults calculateDiffrotProfile(const IPCsettings& set, FITStime& FITS_time, int itersPic, int itersX, int itersY, int itersMedian, int strajdPic, int deltaPic, int verticalFov, int deltaSec, Logger* logger, AbstractPlot1D* pltX, AbstractPlot1D* pltY)
+{
+
+}
