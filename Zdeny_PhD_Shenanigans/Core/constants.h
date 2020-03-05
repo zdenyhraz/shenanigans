09b491dd4991aa0923d64fa7469dc90d18213490
Zdenek Hrazdira <Zdenek.Hrazdira@bagirasys.com>
2020-03-05 11:47:02 +0100

    constants

--- Zdeny_PhD_Shenanigans/Core/constants.h
+++ Zdeny_PhD_Shenanigans/Core/constants.h
@@ -1,9 +1,11 @@
 #pragma once
 
-namespace Constants
-{
-	constexpr double Pi = 3.1415926535897932384626433;
-	constexpr double TwoPi = 2. * Pi;
-	constexpr double HalfPi = 0.5 * Pi;
-	constexpr double E = 2.7182818284590452353602874;
+namespace Constants {
+
+constexpr double Pi = 3.1415926535897932384626433;
+constexpr double TwoPi = Pi * 2;
+constexpr double HalfPi = Pi / 2;
+constexpr double QuartPi = Pi / 4;
+constexpr double E = 2.7182818284590452353602874;
+
 }
