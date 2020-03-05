111c1763b53d421d7f9a91839c97318619734ebd
Zdenek Hrazdira <Zdenek.Hrazdira@bagirasys.com>
2020-03-05 11:36:09 +0100

    plot with only y (iota) support

--- Zdeny_PhD_Shenanigans/Plot/plotter.h
+++ Zdeny_PhD_Shenanigans/Plot/plotter.h
@@ -4,6 +4,7 @@
 struct IPlot1D
 {
 	// x - y
+	inline virtual void plot( const std::vector<double> &y ) = 0;
 	inline virtual void plot( const std::vector<double> &x, const std::vector<double> &y ) = 0;
 	inline virtual void plot( const double x, const double y ) = 0;
 	inline virtual void setAxisNames( std::string xlabel, std::string ylabel ) = 0;
@@ -13,7 +14,7 @@ struct IPlot1D
 	inline virtual void plot( const double x, const double y1, const double y2 ) = 0;
 	inline virtual void setAxisNames( std::string xlabel, std::string ylabel1, std::string ylabel2 ) = 0;
 
-	// x - yn...
+	// x - y1/y2...yn
 	inline virtual void plot( const std::vector<double> &x, const std::vector<std::vector<double>> &ys ) = 0;
 	inline virtual void setAxisNames( std::string xlabel, std::string ylabel, std::vector<std::string> ylabels ) = 0;
 
