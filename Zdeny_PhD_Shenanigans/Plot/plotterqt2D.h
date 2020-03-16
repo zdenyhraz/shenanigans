ec51f16a977a0be4410d1fc26c7fb9521ba33236
Zdenek Hrazdira <Zdenek.Hrazdira@bagirasys.com>
2020-03-16 18:10:22 +0100

    Plot2D shared ptrs

--- Zdeny_PhD_Shenanigans/Plot/plotterqt2D.h
+++ Zdeny_PhD_Shenanigans/Plot/plotterqt2D.h
@@ -11,7 +11,7 @@
 
 struct Plot2D : IPlot2D
 {
-	std::vector<WindowPlot *> windowPlots;
+	std::vector<std::shared_ptr<WindowPlot>> windowPlots;
 
 	inline Plot2D()
 	{
@@ -25,26 +25,26 @@ struct Plot2D : IPlot2D
 
 	inline void plot( const std::vector<std::vector<double>> &z, std::string xlabel = "x", std::string ylabel = "y", std::string zlabel = "z", double xmin = 0, double xmax = 1, double ymin = 0, double ymax = 1 ) override
 	{
-		windowPlots.push_back( new WindowPlot() );
+		windowPlots.push_back( std::make_shared<WindowPlot>() );
 		int index = windowPlots.size() - 1;
 		windowPlots[index]->show();
 		windowPlots[index]->ui.widget->addGraph();//create graph
 		windowPlots[index]->ui.widget->axisRect()->setupFullAxesBox( true ); //configure axis rect
 		windowPlots[index]->ui.widget->xAxis->setLabel( QString::fromStdString( xlabel ) ); //give the axes some labels
 		windowPlots[index]->ui.widget->yAxis->setLabel( QString::fromStdString( ylabel ) ); //give the axes some labels
-		QCPColorMap *colorMap = new QCPColorMap( windowPlots[index]->ui.widget->xAxis, windowPlots[index]->ui.widget->yAxis ); //set up the QCPColorMap
+		std::shared_ptr<QCPColorMap> colorMap = std::make_shared<QCPColorMap>( windowPlots[index]->ui.widget->xAxis, windowPlots[index]->ui.widget->yAxis ); //set up the QCPColorMap
 		colorMap->data()->setSize( z[0].size(), z.size() ); //we want the color map to have nx * ny data points
 		colorMap->data()->setRange( QCPRange( xmin, xmax ), QCPRange( ymin, ymax ) ); //and span the coordinate range in both key (x) and value (y) dimensions
 		windowPlots[index]->ui.widget->setInteractions( QCP::iRangeDrag | QCP::iRangeZoom ); //allow user to drag axis ranges with mouse, zoom with mouse wheel and select graphs by clicking
-		QCPColorScale *colorScale = new QCPColorScale( windowPlots[index]->ui.widget ); //add a color scale
-		windowPlots[index]->ui.widget->plotLayout()->addElement( 0, 1, colorScale ); //add it to the right of the main axis rect
+		std::shared_ptr<QCPColorScale> colorScale = std::make_shared<QCPColorScale>( windowPlots[index]->ui.widget ); //add a color scale
+		windowPlots[index]->ui.widget->plotLayout()->addElement( 0, 1, colorScale.get() ); //add it to the right of the main axis rect
 		colorScale->setType( QCPAxis::atRight ); //scale shall be vertical bar with tick/axis labels right (actually atRight is already the default)
-		colorMap->setColorScale( colorScale ); //associate the color map with the color scale
+		colorMap->setColorScale( colorScale.get() ); //associate the color map with the color scale
 		colorScale->axis()->setLabel( QString::fromStdString( zlabel ) ); //add the z value name
 		colorMap->setGradient( QCPColorGradient::gpJet ); //set the color gradient of the color map to one of the presets
-		QCPMarginGroup *marginGroup = new QCPMarginGroup( windowPlots[index]->ui.widget ); //make sure the axis rect and color scale synchronize their bottom and top margins (so they line up)
-		windowPlots[index]->ui.widget->axisRect()->setMarginGroup( QCP::msBottom | QCP::msTop, marginGroup ); //align plot
-		colorScale->setMarginGroup( QCP::msBottom | QCP::msTop, marginGroup ); //align colorbar
+		std::shared_ptr<QCPMarginGroup> marginGroup = std::make_shared<QCPMarginGroup>( windowPlots[index]->ui.widget ); //make sure the axis rect and color scale synchronize their bottom and top margins (so they line up)
+		windowPlots[index]->ui.widget->axisRect()->setMarginGroup( QCP::msBottom | QCP::msTop, marginGroup.get() ); //align plot
+		colorScale->setMarginGroup( QCP::msBottom | QCP::msTop, marginGroup.get() ); //align colorbar
 		colorMap->setInterpolate( true ); //interpolate bro
 		windowPlots[index]->ui.widget->xAxis->setTickLabelFont( fontTicks );
 		windowPlots[index]->ui.widget->yAxis->setTickLabelFont( fontTicks );
