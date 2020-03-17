78ca0b09ecea3425388ddc0a1703ccefd4c240b1
Zdenek Hrazdira <Zdenek.Hrazdira@bagirasys.com>
2020-03-17 16:28:56 +0100

    Plot1D infra 2

--- Zdeny_PhD_Shenanigans/Plot/Plot1D.cpp
+++ Zdeny_PhD_Shenanigans/Plot/Plot1D.cpp
@@ -5,11 +5,41 @@ std::map<std::string, WindowPlot *> Plot1D::plots;
 
 std::function<void( std::string )> Plot1D::OnClose = []( std::string name )
 {
-
+	auto idx = plots.find( name );
+	if ( idx != plots.end() )
+	{
+		LOG_DEBUG( "Deleting 1Dplot '{}'", name );
+		delete idx->second;
+		plots.erase( idx );
+	}
+	else
+	{
+		LOG_ERROR( "1Dplot '{}' not found in plot registry (why tho?), not deleting", name );
+	}
 };
 
-void Plot1D::plot( const std::vector<std::vector<double>> &z, std::string name, std::string xlabel, std::string ylabel, std::string zlabel, double xmin, double xmax, double ymin, double ymax, std::string savepath )
+void Plot1D::plot( const std::vector<std::vector<double>> &z, std::string name, std::string xlabel, std::string ylabel, double xmin, double xmax, std::string savepath )
 {
+	WindowPlot *windowPlot;
+
+	auto idx = plots.find( name );
+	if ( idx != plots.end() )
+	{
+		LOG_DEBUG( "Updating 1Dplot '{}'", name );
+		windowPlot = idx->second;
+		windowPlot->Clear();
+	}
+	else
+	{
+		LOG_DEBUG( "Creating 1Dplot '{}'", name );
+		windowPlot = new WindowPlot( name, OnClose );
+	}
+	plots[name] = windowPlot;
+
+
+
+
+
 
 }
 
--- Zdeny_PhD_Shenanigans/Plot/Plot1D.h
+++ Zdeny_PhD_Shenanigans/Plot/Plot1D.h
@@ -8,12 +8,7 @@ class Plot1D
 {
 public:
 
-	static void plot( const Mat &z, std::string name, std::string xlabel = "x", std::string ylabel = "y", std::string zlabel = "z", double xmin = 0, double xmax = 1, double ymin = 0, double ymax = 1, std::string savepath = "" )
-	{
-		return plot( matToVect2( z ), name, xlabel, ylabel, zlabel, xmin, xmax, ymin, ymax, savepath );
-	}
-
-	static void plot( const std::vector<std::vector<double>> &z, std::string name, std::string xlabel = "x", std::string ylabel = "y", std::string zlabel = "z", double xmin = 0, double xmax = 1, double ymin = 0, double ymax = 1, std::string savepath = "" );
+	static void plot( const std::vector<std::vector<double>> &z, std::string name, std::string xlabel = "x", std::string ylabel = "y", double xmin = 0, double xmax = 1, std::string savepath = "" );
 
 private:
 	static std::map<std::string, WindowPlot *> plots;
