f375bdc638b8e47343fc2dc6f70397188367dcbd
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-03-18 09:48:22 +0100

    comments

--- Zdeny_PhD_Shenanigans/Plot/Plot1D.h
+++ Zdeny_PhD_Shenanigans/Plot/Plot1D.h
@@ -4,9 +4,9 @@
 #include "Gui/WindowPlot.h"
 #include "Plot.h"
 
-// TODO: add addData support
-// widget->graph( 0 )->addData( x, y1 );
-// widget->graph(1)->addData(x, y2);
+// TODO: add data add dataData support
+// widget->graph( 0 )->add dataData( x, y1 );
+// widget->graph(1)->add dataData(x, y2);
 using namespace Constants;
 
 class Plot1D
@@ -17,31 +17,31 @@ public:
 
 	static void SetupGraph( WindowPlot *windowPlot, int ycnt, int y1cnt, int y2cnt, std::string xlabel, std::string y1label, std::string y2label, std::vector<std::string> &y1names, std::vector<std::string> &y2names );
 
-	// x - y ADD
+	// x - y add data
 	static void plot( double x, double y, std::string name, std::string xlabel = "x", std::string ylabel = "y", std::string savepath = emptystring )
 	{
 		return plotinsides( x, std::vector<double> {y}, emptyvect, name, xlabel, ylabel, emptystring, emptyvectstring, emptyvectstring, savepath );
 	}
 
-	// x - y1/y2 ADD
+	// x - y1/y2 add data
 	static void plot( double x, double y1, double y2, std::string name, std::string xlabel = "x", std::string y1label = "y1", std::string y2label = "y2", std::string savepath = emptystring )
 	{
 		return plotinsides( x, std::vector<double> {y1}, std::vector<double> {y2}, name, xlabel, y1label, y2label, emptyvectstring, emptyvectstring, savepath );
 	}
 
-	// x - ys ADD
+	// x - ys add data
 	static void plot( double x, const std::vector<double> &ys, std::string name, std::string xlabel = "x", std::string ylabel = "y", std::vector<std::string> ynames = emptyvectstring, std::string savepath = emptystring )
 	{
 		return plotinsides( x, ys, emptyvect, name, xlabel, ylabel, emptystring, ynames, emptyvectstring, savepath );
 	}
 
-	// x - y1s/y2s ADD
+	// x - y1s/y2s add data
 	static void plot( double x, const std::vector<double> &y1s, const std::vector<double> &y2s, std::string name, std::string xlabel = "x", std::string y1label = "y1", std::string y2label = "y2", std::vector<std::string> y1names = emptyvectstring, std::vector<std::string> y2names = emptyvectstring, std::string savepath = emptystring )
 	{
 		return plotinsides( x, y1s, y2s, name, xlabel, y1label, y2label, y1names, y2names, savepath );
 	}
 
-	// iota - y REPLOT
+	// iota - y replot data
 	static void plot( const std::vector<double> &y, std::string name, std::string xlabel = "x", std::string ylabel = "y", std::string savepath = emptystring )
 	{
 		std::vector<double> x( y.size() );
@@ -49,34 +49,34 @@ public:
 		return plotinsides( x, std::vector<std::vector<double>> {y}, emptyvect2, name, xlabel, ylabel, emptystring, emptyvectstring, emptyvectstring, savepath );
 	}
 
-	// x - y REPLOT
+	// x - y replot data
 	static void plot( const std::vector<double> &x, const std::vector<double> &y, std::string name, std::string xlabel = "x", std::string ylabel = "y", std::string savepath = emptystring )
 	{
 		return plotinsides( x, std::vector<std::vector<double>> {y}, emptyvect2, name, xlabel, ylabel, emptystring, emptyvectstring, emptyvectstring, savepath );
 	}
 
-	// x - y1/y2 REPLOT
+	// x - y1/y2 replot data
 	static void plot( const std::vector<double> &x, const std::vector<double> &y1, const std::vector<double> &y2, std::string name, std::string xlabel = "x", std::string y1label = "y1", std::string y2label = "y1", std::string savepath = emptystring )
 	{
 		return plotinsides( x, std::vector<std::vector<double>> {y1}, std::vector<std::vector<double>> {y2}, name, xlabel, y1label, y2label, emptyvectstring, emptyvectstring, savepath );
 	}
 
-	// x - ys REPLOT
+	// x - ys replot data
 	static void plot( const std::vector<double> &x, const std::vector<std::vector<double>> &ys, std::string name, std::string xlabel = "x", std::string ylabel = "y", std::vector<std::string> ynames = emptyvectstring, std::string savepath = emptystring )
 	{
 		return plotinsides( x, ys, emptyvect2, name, xlabel, ylabel, emptystring, ynames, emptyvectstring, savepath );
 	}
 
-	// x - ys1/ys2 REPLOT
+	// x - ys1/ys2 replot data
 	static void plot( const std::vector<double> &x, const std::vector<std::vector<double>> &y1s, const std::vector<std::vector<double>> &y2s, std::string name, std::string xlabel = "x", std::string y1label = "y1", std::string y2label = "y2", std::vector<std::string> y1names = emptyvectstring, std::vector<std::string> y2names = emptyvectstring, std::string savepath = emptystring )
 	{
 		return plotinsides( x, y1s, y2s, name, xlabel, y1label, y2label, y1names, y2names, savepath );
 	}
 
-	// core REPLOT
+	// replot data core
 	static void plotinsides( const std::vector<double> &x, const std::vector<std::vector<double>> &y1s, const std::vector<std::vector<double>> &y2s, std::string name, std::string xlabel = "x", std::string y1label = "y1", std::string y2label = "y2", std::vector<std::string> y1names = emptyvectstring, std::vector<std::string> y2names = emptyvectstring, std::string savepath = emptystring );
 
-	// core ADD
+	// add data core
 	static void plotinsides( double x, const std::vector<double> &y1s, const std::vector<double> &y2s, std::string name, std::string xlabel = "x", std::string y1label = "y1", std::string y2label = "y2", std::vector<std::string> y1names = emptyvectstring, std::vector<std::string> y2names = emptyvectstring, std::string savepath = emptystring );
 
 private:
