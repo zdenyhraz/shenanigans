46799885b1d00c323c65f5b59dae871b3814b1a5
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-03-19 10:35:36 +0100

    constantsd

--- Zdeny_PhD_Shenanigans/Core/constants.h
+++ Zdeny_PhD_Shenanigans/Core/constants.h
@@ -1,14 +1,14 @@
 #pragma once
 
 namespace Constants {
-constexpr double Pi = 3.1415926535897932384626433;
-constexpr double TwoPi = Pi * 2;
-constexpr double HalfPi = Pi / 2;
-constexpr double QuartPi = Pi / 4;
-constexpr double E = 2.7182818284590452353602874;
-constexpr double Max = std::numeric_limits<double>::max();
-constexpr double Min = std::numeric_limits<double>::lowest();
-constexpr double Eps = std::numeric_limits<double>::epsilon();
+static constexpr double Pi = 3.1415926535897932384626433;
+static constexpr double TwoPi = Pi * 2;
+static constexpr double HalfPi = Pi / 2;
+static constexpr double QuartPi = Pi / 4;
+static constexpr double E = 2.7182818284590452353602874;
+static constexpr double Max = std::numeric_limits<double>::max();
+static constexpr double Min = std::numeric_limits<double>::lowest();
+static constexpr double Eps = std::numeric_limits<double>::epsilon();
 
 static const std::vector<double> emptyvect = std::vector<double> {};
 static const std::vector<std::vector<double>> emptyvect2 = std::vector<std::vector<double>> {};
