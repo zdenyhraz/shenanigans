ead8e65a73142289979e9f50a25c42b9add15080
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-03-25 14:16:14 +0100

    separate Swind flow, remove old diffrot

--- Zdeny_PhD_Shenanigans/Gui/Windows/WindowDiffrot.cpp
+++ Zdeny_PhD_Shenanigans/Gui/Windows/WindowDiffrot.cpp
@@ -11,7 +11,6 @@ WindowDiffrot::WindowDiffrot( QWidget *parent, Globals *globals ) : QMainWindow(
 	connect( ui.pushButton_2, SIGNAL( clicked() ), this, SLOT( showResults() ) );
 	connect( ui.pushButton_3, SIGNAL( clicked() ), this, SLOT( showIPC() ) );
 	connect( ui.pushButton_4, SIGNAL( clicked() ), this, SLOT( optimizeDiffrot() ) );
-	connect( ui.pushButton_5, SIGNAL( clicked() ), this, SLOT( superOptimizeDiffrot() ) );
 }
 
 void WindowDiffrot::calculateDiffrot()
@@ -46,8 +45,7 @@ void WindowDiffrot::showIPC()
 	FitsParams params1, params2;
 	IPCsettings set = *globals->IPCset;
 	set.broadcast = true;
-	FitsTime fitsTime( ui.lineEdit_17->text().toStdString(), ui.lineEdit_10->text().toInt(), ui.lineEdit_11->text().toInt(), ui.lineEdit_12->text().toInt(),
-	                   ui.lineEdit_13->text().toInt(), ui.lineEdit_14->text().toInt(), ui.lineEdit_15->text().toInt() );
+	FitsTime fitsTime( ui.lineEdit_17->text().toStdString(), ui.lineEdit_10->text().toInt(), ui.lineEdit_11->text().toInt(), ui.lineEdit_12->text().toInt(), ui.lineEdit_13->text().toInt(), ui.lineEdit_14->text().toInt(), ui.lineEdit_15->text().toInt() );
 	LOG_INFO( "Loading file '" + fitsTime.path() + "'..." );
 	Mat pic1 = roicrop( loadfits( fitsTime.path(), params1 ), params1.fitsMidX, params1.fitsMidY, set.getcols(), set.getrows() );
 	fitsTime.advanceTime( ui.lineEdit_5->text().toDouble()*ui.lineEdit_8->text().toDouble() );
@@ -59,62 +57,7 @@ void WindowDiffrot::showIPC()
 
 void WindowDiffrot::optimizeDiffrot()
 {
-	FitsParams params1, params2;
-	FitsTime fitsTime( ui.lineEdit_17->text().toStdString(), ui.lineEdit_10->text().toInt(), ui.lineEdit_11->text().toInt(), ui.lineEdit_12->text().toInt(), ui.lineEdit_13->text().toInt(), ui.lineEdit_14->text().toInt(), ui.lineEdit_15->text().toInt() );
-	std::vector<int> sizes{ 16, 32, 64, 128 };
-	std::string path = "D:\\MainOutput\\diffrot\\diffrotIPCopt.csv";
-	std::ofstream listing( path, std::ios::out | std::ios::trunc ); //just delete
-	for ( auto &size : sizes )
-	{
-		LOG_INFO( "Optimizing IPC parameters for diffrot profile measurement " + to_string( size ) + "x" + to_string( size ) + "..." );
-		IPCsettings set = *globals->IPCset;
-		set.setSize( size, size );
-		optimizeIPCParameters( set, fitsTime.path(), path, 5, 0.01, 3 );
-	}
-	LOG_INFO( "IPC parameters optimization for diffrot profile measurement finished" );
+
 }
 
-void WindowDiffrot::superOptimizeDiffrot()
-{
-	/*
-	// size, Lm, Hm, L2, W
-	Evolution Evo( 5 );
-	Evo.lowerBounds = std::vector<double> { 10, 0, 0, 5, -1 };
-	Evo.upperBounds = std::vector<double> { 256, 10, 500, 15, 1 };
-	*/
-
-	// Lm, Hm
-	Evolution Evo( 2 );
-	Evo.lowerBounds = std::vector<double> {  0, 0 };
-	Evo.upperBounds = std::vector<double> { 10, 500 };
-
-	Evo.NP = 50;
-	Evo.mutStrat = Evolution::MutationStrategy::BEST1;
-
-	LOG_INFO( "Super optimizing diffrot profile..." );
-
-	FitsTime fitsTimeMaster( ui.lineEdit_17->text().toStdString(), ui.lineEdit_10->text().toInt(), ui.lineEdit_11->text().toInt(), ui.lineEdit_12->text().toInt(),
-	                         ui.lineEdit_13->text().toInt(), ui.lineEdit_14->text().toInt(), ui.lineEdit_15->text().toInt() );
-
-	std::vector<std::pair<FitsImage, FitsImage>> pics;
-	double dt = 45;
-	for ( int i = 0; i < 3; i++ )
-	{
-		FitsImage a( fitsTimeMaster.path() );
-		fitsTimeMaster.advanceTime( dt );
-
-		FitsImage b( fitsTimeMaster.path() );
-
-		if ( a.params().succload && b.params().succload )
-			pics.emplace_back( std::make_pair( a, b ) );
-	}
-
-	//auto f = [&]( std::vector<double> arg ) { return DiffrotMerritFunctionWrapper( arg, pics, ui.lineEdit->text().toDouble(), ui.lineEdit_2->text().toDouble(), ui.lineEdit_3->text().toDouble(), ui.lineEdit_6->text().toDouble(), ui.lineEdit_5->text().toDouble(), ui.lineEdit_4->text().toDouble(), ui.lineEdit_8->text().toDouble() ); };
-	auto f2 = [&]( std::vector<double> arg ) { return DiffrotMerritFunctionWrapper2( arg, pics, ui.lineEdit->text().toDouble(), ui.lineEdit_2->text().toDouble(), ui.lineEdit_3->text().toDouble(), ui.lineEdit_6->text().toDouble(), ui.lineEdit_5->text().toDouble(), ui.lineEdit_4->text().toDouble(), ui.lineEdit_8->text().toDouble() ); };
-
-	auto result = Evo.optimize( f2 );
-	LOG_SUCC( "Optimal Lmult: " + to_string( result[0] ) );
-	LOG_SUCC( "Optimal Hmult: " + to_string( result[1] ) );
-	LOG_INFO( "Super optimizing finished for dt={}`", dt );
-}
 
