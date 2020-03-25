bc00b6c9a316fa1b77bb8608d7aff9428c24d652
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-03-25 14:23:20 +0100

    remove inline .cpps

--- Zdeny_PhD_Shenanigans/Core/functionsBaseSTL.cpp
+++
@@ -1,7 +0,0 @@
-//auxiliary functions and features
-//updated frequently @ https://github.com/zdenyhraz
-//PhD work of Zdenek Hrazdira
-//made during 2018-2019
-
-#include "stdafx.h"
-#include "Core/functionsBaseSTL.h"
