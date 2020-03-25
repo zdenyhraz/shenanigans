36fa3591f7adaf3ca1b9fbaffde519d0fa49e42f
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-03-25 16:00:02 +0100

    refactor diffrot results

--- Zdeny_PhD_Shenanigans/Astrophysics/diffrotResults.h
+++ Zdeny_PhD_Shenanigans/Astrophysics/diffrotResults.h
@@ -21,46 +21,25 @@ public:
 		}
 
 		// diffrot profile
-		if ( 1 )
-		{
-			Plot1D::plot( SourceThetas, std::vector<std::vector<double>> {SourceOmegasXavg, SourceOmegasXavgpolyfit, SourceOmegasXavgsin2sin4fit, SourcePredicX1, SourcePredicX2}, "diffrot profile X", "solar latitude [deg]", "horizontal plasma flow speed [deg/day]", std::vector<std::string> {"omegasXavg", "omegasXavgpolyfit", "omegasXavgsin2sin4fit", "predicX1", "predicX2"}, saveDir + "diffrot profile X.png" );
-		}
+		Plot1D::plot( SourceThetas, std::vector<std::vector<double>> {SourceOmegasXavg, SourceOmegasXavgpolyfit, SourceOmegasXavgsin2sin4fit, SourcePredicX1, SourcePredicX2}, "diffrot profile X", "solar latitude [deg]", "horizontal plasma flow speed [deg/day]", std::vector<std::string> {"omegasXavg", "omegasXavgpolyfit", "omegasXavgsin2sin4fit", "predicX1", "predicX2"}, saveDir + "diffrot profile X.png" );
 
 		// shifts profile
-		if ( 1 )
-		{
-			Plot1D::plot( SourceThetas, SourceShiftsX, "shifts profile X", "solar latitude [deg]", "horizontal px shift [px]", saveDir + "shifts profile X.png" );
-		}
+		Plot1D::plot( SourceThetas, SourceShiftsX, "shifts profile X", "solar latitude [deg]", "horizontal px shift [px]", saveDir + "shifts profile X.png" );
 
 		// flow X jet
-		if ( 1 )
-		{
-			Plot2D::plot( applyQuantile( FlowX, quanBot, quanTop ), "diffrot flow X", "time [hours]", "solar latitude [deg]", "horizontal plasma flow speed [deg/day]", 0, ( double )( FlowX.cols - 1 ) * SourceStride * 45 / 60 / 60, SourceThetas.front(), SourceThetas.back(), colRowRatio, saveDir + to_string( medianSize ) + "_diffrot flow X.png" );
-		}
+		Plot2D::plot( applyQuantile( FlowX, quanBot, quanTop ), "diffrot flow X", "time [hours]", "solar latitude [deg]", "horizontal plasma flow speed [deg/day]", 0, ( double )( FlowX.cols - 1 ) * SourceStride * 45 / 60 / 60, SourceThetas.front(), SourceThetas.back(), colRowRatio, saveDir + to_string( medianSize ) + "_diffrot flow X.png" );
 
 		// relative flow X jet
-		if ( 1 )
-		{
-			Plot2D::plot( applyQuantile( FlowX - SourcePredicX, quanBot, quanTop ), "diffrot relative flow X", "time [hours]", "solar latitude [deg]", "relative horizontal plasma flow speed [deg/day]", 0, ( double )( FlowX.cols - 1 ) * SourceStride * 45 / 60 / 60, SourceThetas.front(), SourceThetas.back(), colRowRatio, saveDir + to_string( medianSize ) + "_diffrot relative flow X.png" );
-		}
+		Plot2D::plot( applyQuantile( FlowX - SourcePredicX, quanBot, quanTop ), "diffrot relative flow X", "time [hours]", "solar latitude [deg]", "relative horizontal plasma flow speed [deg/day]", 0, ( double )( FlowX.cols - 1 ) * SourceStride * 45 / 60 / 60, SourceThetas.front(), SourceThetas.back(), colRowRatio, saveDir + to_string( medianSize ) + "_diffrot relative flow X.png" );
 
 		// source img
-		if ( 1 )
-		{
-			showimg( SourceImage, "diffrot source" );
-		}
+		showimg( SourceImage, "diffrot source" );
 
 		// relative flow X binary
-		if ( 1 )
-		{
-			showimg( combineTwoPics( FlowX - SourcePredicX, SourceImage, bluered, sigma ), "diffrot relative flow X binary", false, quanBot, quanTop );
-		}
+		showimg( combineTwoPics( FlowX - SourcePredicX, SourceImage, bluered, sigma ), "diffrot relative flow X binary", false, quanBot, quanTop );
 
 		// relative magnitude & absolute phase
-		if ( 1 )
-		{
-			// needs Y information
-		}
+
 	}
 
 	void SetData2D( const std::vector<std::vector<double>> &image, const std::vector<std::vector<double>> &flowX, const std::vector<std::vector<double>> &predicX )
@@ -87,11 +66,12 @@ public:
 	}
 
 private:
+
+	// source data
 	Mat SourceImage;
 	Mat SourceFlowX;
 	Mat SourcePredicX;
 	int SourceStride;
-
 	std::vector<double> SourceThetas;
 	std::vector<double> SourceOmegasXavg;
 	std::vector<double> SourceOmegasXavgpolyfit;
@@ -100,6 +80,7 @@ private:
 	std::vector<double> SourcePredicX2;
 	std::vector<double> SourceShiftsX;
 
+	// internal data
 	Mat FlowX;
 	std::string saveDir = "C:\\Users\\Zdeny\\Desktop\\PhD_things\\diffrot\\plotsave\\";
 	static constexpr double colRowRatio = 2;
