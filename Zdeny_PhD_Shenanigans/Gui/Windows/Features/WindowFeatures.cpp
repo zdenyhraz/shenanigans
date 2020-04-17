41ce7924e5da07925ef509b88f52120399eadab9
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-04-17 11:40:41 +0200

    features 3

--- Zdeny_PhD_Shenanigans/Gui/Windows/Features/WindowFeatures.cpp
+++ Zdeny_PhD_Shenanigans/Gui/Windows/Features/WindowFeatures.cpp
@@ -11,7 +11,10 @@ void WindowFeatures::FeatureMatch()
 {
 	LOG_INFO( "Matching features..." );
 
+	std::string path1 = ui.lineEdit->text().toStdString();
+	std::string path2 = ui.lineEdit_2->text().toStdString();
 
+	featureMatch( path1, path2 );
 
 
 
