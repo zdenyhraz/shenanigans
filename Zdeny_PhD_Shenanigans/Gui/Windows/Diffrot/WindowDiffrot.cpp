44250d09342561cd4938088bd2900da72d232e74
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-09-17 11:19:39 +0200

    loading safety

--- Zdeny_PhD_Shenanigans/Gui/Windows/Diffrot/WindowDiffrot.cpp
+++ Zdeny_PhD_Shenanigans/Gui/Windows/Diffrot/WindowDiffrot.cpp
@@ -123,11 +123,11 @@ void WindowDiffrot::checkDiskShifts()
   Plot1D::plot(iotam, std::vector<std::vector<double>>{shiftsFY, shiftsN, shiftsS}, "shiftsY", "time [days]", "45sec shiftY [px]", std::vector<std::string>{"shifts fits header Y", "shifts IPC north edge", "shifts IPC south edge"});
 }
 
-void WindowDiffrot::saveDiffrot() { SaveDiffrotResultsToFile(ui.lineEdit_9->text().toStdString(), ui.lineEdit_23->text().toStdString(), &drres); }
+void WindowDiffrot::saveDiffrot() { SaveDiffrotResultsToFile(ui.lineEdit_9->text().toStdString(), ui.lineEdit_23->text().toStdString(), &drres, globals->IPCset.get()); }
 
 void WindowDiffrot::loadDiffrot()
 {
-  LoadDiffrotResultsFromFile(ui.lineEdit_24->text().toStdString(), &drres);
+  LoadDiffrotResultsFromFile(ui.lineEdit_24->text().toStdString(), &drres, globals->IPCset.get());
   drres.saveDir = ui.lineEdit_9->text().toStdString();
 }
 
