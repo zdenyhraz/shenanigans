e75192d45a51f0828daa7462f40f3b569f988808
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-09-18 22:16:44 +0200

    vids

--- Zdeny_PhD_Shenanigans/Astrophysics/diffrot.cpp
+++ Zdeny_PhD_Shenanigans/Astrophysics/diffrot.cpp
@@ -44,7 +44,7 @@ DiffrotResults calculateDiffrotProfile(const IterativePhaseCorrelation &ipc, Fit
     {
       if (drset.video)
       {
-        Mat crop = roicrop(pic1.image(), pic1.params().fitsMidX, pic1.params().fitsMidY - 0.7 * pic1.params().R, 256, 256);
+        Mat crop = roicrop(pic1.image(), pic1.params().fitsMidX, pic1.params().fitsMidY - 0.7 * pic1.params().R, 100, 100);
         normalize(crop, crop, 0, 65535, NORM_MINMAX);
         saveimg(drset.savepath + to_string(pic) + ".png", crop, true, cv::Size(1000, 1000));
         continue;
