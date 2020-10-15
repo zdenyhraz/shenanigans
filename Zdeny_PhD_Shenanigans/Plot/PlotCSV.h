ed9c25482fdbab20259a363cfca8a30384d02c50
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-10-15 15:02:51 +0200

    inconsistent columsn csv

--- Zdeny_PhD_Shenanigans/Plot/PlotCSV.h
+++ Zdeny_PhD_Shenanigans/Plot/PlotCSV.h
@@ -5,25 +5,21 @@
 class PlotCSV
 {
 public:
-  static void plot(const std::string &path)
+  static void plot(const std::string &path, const std::string &savepath = "")
   {
-    auto [data, labels] = ParseCSV(path);
+    auto [x, ys, xlabel, ylabels] = ParseCSV(path);
 
-    if (data.empty() || labels.empty())
+    if (x.empty() || ys.empty())
       return;
 
     std::string plotname = "token count";
-    std::vector<double> x = data[0];
-    std::vector<std::vector<double>> ys = {data.begin() + 1, data.end()};
-    std::vector<std::string> ylabels = {labels.begin() + 1, labels.end()};
-    std::string xlabel = labels[0];
     std::string ylabel = plotname;
 
-    Plot1D::plot(x, ys, plotname, xlabel, ylabel, ylabels, Plot::defaultpens, "E:\\Zdeny_PhD_Shenanigans\\articles\\tokens\\plot.png");
+    Plot1D::plot(x, ys, plotname, xlabel, ylabel, ylabels, Plot::defaultpens, savepath);
   }
 
 private:
-  static std::tuple<std::vector<std::vector<double>>, std::vector<std::string>> ParseCSV(const std::string &path)
+  static std::tuple<std::vector<double>, std::vector<std::vector<double>>, std::string, std::vector<std::string>> ParseCSV(const std::string &path)
   {
     std::ifstream file(path);
     if (!file.is_open() || !file.good())
@@ -32,19 +28,20 @@ private:
       return {};
     }
 
-    std::vector<std::vector<double>> data;
+    // fill label vector from first line
     std::vector<std::string> labels;
     std::string line, colname;
-    double value;
-
-    // fill label vector from first line
     std::getline(file, line);
     std::stringstream ss(line);
     while (std::getline(ss, colname, ','))
       labels.push_back(colname);
 
-    // the data dimension is the same as the label dimension
-    data.resize(labels.size());
+    // fill x & ys vectors from the rest of the file
+    std::vector<std::string> ylabels{labels.begin() + 1, labels.end()};
+    std::vector<std::vector<double>> ys(ylabels.size());
+    std::string xlabel = labels[0];
+    std::vector<double> x;
+    double value;
 
     while (std::getline(file, line))
     {
@@ -53,15 +50,24 @@ private:
 
       while (ss >> value)
       {
-        data[col].push_back(value);
+        if (!col)
+          x.push_back(value);
+        else
+          ys[col - 1].push_back(value);
 
         if (ss.peek() == ',')
           ss.ignore();
 
         col++;
       }
+
+      if (col != ys.size() + 1)
+      {
+        LOG_ERROR("Inconsistent csv columns for x={}: {}!={}", x.back(), col, ys.size());
+        return {};
+      }
     }
 
-    return {data, labels};
+    return {x, ys, xlabel, ylabels};
   }
 };
