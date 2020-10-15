8003803301cd5be7578cac1c6285edf559695f04
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-10-15 13:54:55 +0200

    csv plot refactoring

--- Zdeny_PhD_Shenanigans/Plot/PlotCSV.h
+++ Zdeny_PhD_Shenanigans/Plot/PlotCSV.h
@@ -11,13 +11,13 @@ public:
   {
     auto [data, labels] = ParseCSV(path);
 
-    if (labels.empty() || data.empty())
+    if (data.empty() || labels.empty())
       return;
 
+    std::string plotname = "token count";
     std::vector<double> x = data[0];
     std::vector<std::vector<double>> ys = {data.begin() + 1, data.end()};
     std::vector<std::string> ylabels = {labels.begin() + 1, labels.end()};
-    std::string plotname = "token count";
     std::string xlabel = labels[0];
     std::string ylabel = plotname;
 
@@ -27,34 +27,35 @@ public:
 private:
   static std::tuple<std::vector<std::vector<double>>, std::vector<std::string>> ParseCSV(const std::string &path)
   {
-    std::vector<std::vector<double>> data;
-    std::vector<std::string> labels;
-
-    std::ifstream myFile(path);
-
-    if (!myFile.is_open() || !myFile.good())
+    std::ifstream file(path);
+    if (!file.is_open() || !file.good())
     {
       LOG_ERROR("Could not open file {}", path);
       return {};
     }
 
+    std::vector<std::vector<double>> data;
+    std::vector<std::string> labels;
     std::string line, colname;
-    double val;
+    double value;
 
-    std::getline(myFile, line);
+    // fill label vector from first line
+    std::getline(file, line);
     std::stringstream ss(line);
     while (std::getline(ss, colname, ','))
       labels.push_back(colname);
 
+    // the data dimension is the same as the label dimension
     data.resize(labels.size());
-    while (std::getline(myFile, line))
+
+    while (std::getline(file, line))
     {
       std::stringstream ss(line);
       int col = 0;
 
-      while (ss >> val)
+      while (ss >> value)
       {
-        data[col].push_back(val);
+        data[col].push_back(value);
 
         if (ss.peek() == ',')
           ss.ignore();
