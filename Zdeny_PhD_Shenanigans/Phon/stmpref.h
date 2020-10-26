015e5f6721ef7f40afae6969f164c8a491b7dd08
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-10-26 10:13:32 +0100

    phon

---
+++ Zdeny_PhD_Shenanigans/Phon/stmpref.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "stdafx.h"
+
+void ExtractTrigramsFromStmToFile(const std::string &stmfilepath, const std::string &unkwordsfilepath, const std::string &outputfilepath)
+{
+  // search through the STM file for all occurences of words specified by the UNKWORDS file and output corresponding STM trigrams to the OUTPUT file
+
+  // check validity of files
+  std::ifstream stmfile(stmfilepath);
+  if (!stmfile.is_open() || !stmfile.good())
+  {
+    LOG_ERROR("Could not open file {}", stmfilepath);
+    return;
+  }
+
+  std::ifstream unkwordsfile(unkwordsfilepath);
+  if (!unkwordsfile.is_open() || !unkwordsfile.good())
+  {
+    LOG_ERROR("Could not open file {}", unkwordsfilepath);
+    return;
+  }
+}
