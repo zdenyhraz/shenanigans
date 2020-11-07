6ff1f2c0a9dc6bbf33392002572d7cbb7224d457
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-11-07 17:34:21 +0100

    matlab plot colors

--- Zdeny_PhD_Shenanigans/Plot/Plot.cpp
+++ Zdeny_PhD_Shenanigans/Plot/Plot.cpp
@@ -3,9 +3,12 @@
 #include "Gui/Windows/Plot/WindowPlot.h"
 
 std::map<std::string, WindowPlot *> Plot::plots;
-QFont Plot::fontTicks("Newyork", 13);  // 9
-QFont Plot::fontLabels("Newyork", 17); // 12
-double Plot::pt = 2.0;                 // pen thickness
+
+QFont Plot::fontTicks("Newyork", 17);
+QFont Plot::fontLabels("Newyork", 17);
+
+double Plot::pt = 3.0;
+
 QColor Plot::blue(30, 80, 255);
 QColor Plot::red(220, 20, 60);
 QColor Plot::green(119, 182, 48);
@@ -13,7 +16,17 @@ QColor Plot::black(50, 50, 50);
 QColor Plot::orange(255, 165, 0);
 QColor Plot::cyan(64, 224, 208);
 QColor Plot::magenta(150, 0, 150);
-QPen Plot::defaultpen(blue, pt);
+
+QColor Plot::matlabBlue(0, 113.9850, 188.9550);
+QColor Plot::matlabOrange(216.750, 82.875, 24.990);
+QColor Plot::matlabYellow(236.895, 176.970, 31.875);
+QColor Plot::matlabMagenta(125.970, 46.920, 141.780);
+QColor Plot::matlabGreen(118.830, 171.870, 47.940);
+QColor Plot::matlabCyan(76.755, 189.975, 237.915);
+QColor Plot::matlabRed(161.925, 19.890, 46.920);
+
+QPen Plot::defaultpen(matlabBlue, pt);
+
 std::vector<QPen> Plot::defaultpens{
     QPen(blue, pt),
     QPen(orange, pt),
