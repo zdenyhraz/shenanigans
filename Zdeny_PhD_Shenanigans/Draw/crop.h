9d06cf18acc5396d9acaa48e660279b5bf59d8fd
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-11-26 19:55:27 +0100

    crop {}

--- Zdeny_PhD_Shenanigans/Draw/crop.h
+++ Zdeny_PhD_Shenanigans/Draw/crop.h
@@ -5,15 +5,9 @@ inline Mat kirkl(unsigned size)
 {
   Mat kirkl = Mat::zeros(size, size, CV_32F);
   for (int r = 0; r < size; r++)
-  {
     for (int c = 0; c < size; c++)
-    {
       if ((sqr((double)r - floor(size / 2)) + sqr((double)c - floor(size / 2))) < sqr((double)size / 2 + 1))
         kirkl.at<float>(r, c) = 1.;
-      else
-        kirkl.at<float>(r, c) = 0.;
-    }
-  }
   return kirkl;
 }
 
@@ -21,23 +15,13 @@ inline Mat kirkl(int rows, int cols, unsigned radius)
 {
   Mat kirkl = Mat::zeros(rows, cols, CV_32F);
   for (int r = 0; r < rows; r++)
-  {
     for (int c = 0; c < cols; c++)
-    {
       if ((sqr((double)r - floor(rows / 2)) + sqr((double)c - floor(cols / 2))) < sqr(radius))
-      {
         kirkl.at<float>(r, c) = 1.;
-      }
-      else
-      {
-        kirkl.at<float>(r, c) = 0.;
-      }
-    }
-  }
   return kirkl;
 }
 
-inline Mat kirklcrop(const Mat &sourceimgIn, int x, int y, int diameter)
+inline Mat kirklcrop(const Mat& sourceimgIn, int x, int y, int diameter)
 {
   Mat crop = roicrop(sourceimgIn, x, y, diameter, diameter);
   return crop.mul(kirkl(diameter));
