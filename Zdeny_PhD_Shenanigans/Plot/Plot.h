a1dfb0a2a888b51a279bcf8078ae0bb1aecf02b5
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-12-06 15:00:29 +0100

    new plot proto

--- Zdeny_PhD_Shenanigans/Plot/Plot.h
+++ Zdeny_PhD_Shenanigans/Plot/Plot.h
@@ -8,13 +8,13 @@ public:
   static QPoint GetNewPlotPosition(WindowPlot* windowPlot);
 
   static std::map<std::string, WindowPlot*> plots;
+  static std::vector<QPen> pens;
+  static double pt;
+
   static QFont fontTicks;
   static QFont fontLabels;
   static QFont fontLegend;
-  static double pt;
-  static std::vector<QPen> pens;
 
-  // matlab plot colors
   static QColor black;
   static QColor blue;
   static QColor orange;
@@ -23,4 +23,53 @@ public:
   static QColor green;
   static QColor cyan;
   static QColor red;
+
+  enum class LegendPosition
+  {
+    TopRight,
+    BotRight,
+    BotLeft,
+    TopLeft
+  };
+
+  class Plot1D
+  {
+  public:
+    Plot1D(const std::string& name);
+
+    void Plot(const std::vector<double>& x, const std::vector<double>& y);
+    void Plot(const std::vector<double>& x, const std::vector<std::vector<double>>& ys);
+    void Plot(const std::vector<double>& x, const std::vector<double>& y1, const std::vector<double>& y2);
+    void Plot(const std::vector<double>& x, const std::vector<std::vector<double>>& y1s, const std::vector<std::vector<double>>& y2s);
+
+    void Plot(double x, double y);
+    void Plot(double x, const std::vector<double>& ys);
+    void Plot(double x, double y1, double y2);
+    void Plot(double x, const std::vector<double>& y1s, const std::vector<double>& y2s);
+
+    std::string mName = "plot";
+    std::string mXlabel = "x";
+    std::string mY1label = "y";
+    std::string mY2label = "y2";
+    std::vector<std::string> mY1names = {};
+    std::vector<std::string> mY2names = {};
+    std::vector<QPen> mPens = pens;
+    std::string mSavepath = {};
+    bool mLegend = true;
+    LegendPosition mLegendPosition = LegendPosition::TopRight;
+    bool mY1Log = false;
+    bool mY2Log = false;
+
+  private:
+    void PlotCoreReplot(const std::vector<double>& x, const std::vector<std::vector<double>>& y1s, const std::vector<std::vector<double>>& y2s);
+    void PlotCoreAdd(double x, const std::vector<double>& y1s, const std::vector<double>& y2s);
+  };
+
+  class Plot2D
+  {
+  public:
+    void Plot();
+
+  private:
+  };
 };
