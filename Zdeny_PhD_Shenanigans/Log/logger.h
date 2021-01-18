900819a5184f7368457b023f28fafc58626053be
zdenyhraz <hrazdira.zdenek@gmail.com>
2021-01-18 17:08:04 +0100

    format duration

--- Zdeny_PhD_Shenanigans/Log/logger.h
+++ Zdeny_PhD_Shenanigans/Log/logger.h
@@ -66,10 +66,31 @@ class LOG_FUNCTION_IMPL
 
 public:
   LOG_FUNCTION_IMPL(const std::string& funName) : mFunName(funName), mStartTime(clock::now()) { LOG_DEBUG(fmt::format("{} started", mFunName)); }
-  ~LOG_FUNCTION_IMPL() { LOG_DEBUG(fmt::format("{} finished ({} ms)", mFunName, TimeSinceEpoch(clock::now()) - TimeSinceEpoch(mStartTime))); }
+
+  ~LOG_FUNCTION_IMPL()
+  {
+    const auto duration = TimeSinceEpoch(clock::now()) - TimeSinceEpoch(mStartTime);
+    LOG_DEBUG(fmt::format("{} finished ({})", mFunName, FormatDuration(duration)));
+  }
 
 private:
   static constexpr long long TimeSinceEpoch(const time_point& tmp) { return std::chrono::time_point_cast<duration>(tmp).time_since_epoch().count(); }
+
+  static std::string FormatDuration(long long durationms)
+  {
+    static constexpr long long kSecondMs = 1000;
+    static constexpr long long kMinuteMs = kSecondMs * 60;
+    static constexpr long long kHourMs = kMinuteMs * 60;
+
+    if (durationms < kSecondMs)
+      return fmt::format("{} ms", durationms);
+    else if (durationms < kMinuteMs)
+      return fmt::format("{} s", (double)durationms / kSecondMs);
+    else if (durationms < kHourMs)
+      return fmt::format("{} m", (double)durationms / kMinuteMs);
+    else
+      return fmt::format("{} h", (double)durationms / kHourMs);
+  }
 
   std::string mFunName;
   time_point mStartTime;
