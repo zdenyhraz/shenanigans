89d7fba612c7fb5a56d210c2803e8f5e0346787c
zdenyhraz <hrazdira.zdenek@gmail.com>
2021-11-30 09:25:32 +0100

    plot opt path nice

--- Zdeny_PhD_Shenanigans/Optimization/Optimization.cpp
+++ Zdeny_PhD_Shenanigans/Optimization/Optimization.cpp
@@ -8,29 +8,93 @@ OptimizationAlgorithm::OptimizationAlgorithm(int N, const std::string& optname) 
 }
 
 void OptimizationAlgorithm::PlotObjectiveFunctionLandscape(ObjectiveFunction f, const std::vector<double> baseParams, int iters, int xParamIndex, int yParamIndex, double xmin, double xmax,
-    double ymin, double ymax, const std::string& xName, const std::string& yName, const std::string& funName)
+    double ymin, double ymax, const std::string& xName, const std::string& yName, const std::string& funName, OptimizationResult* optResult)
 {
   LOG_FUNCTION("PlotObjectiveFunctionLandscape");
   int rows = iters;
   int cols = iters;
-  auto landscape = zerovect2(rows, cols, 0.0);
-  auto landscapeLog = zerovect2(rows, cols, 0.0);
+  Mat landscape = Mat::zeros(rows, cols, CV_32F);
+  Mat landscapeLog = Mat::zeros(rows, cols, CV_32F);
   std::atomic<int> progress = 0;
 
 #pragma omp parallel for
   for (int r = 0; r < rows; ++r)
   {
-    LOG_INFO("Calculating objective function landscape ({:.1f}%)", (float)progress++ / (rows - 1) * 100);
+    progress++;
+    if ((r % (rows / 20) == 0) || r == (rows - 1))
+      LOG_INFO("Drawing objective function landscape ({:.1f}%)", (float)progress / rows * 100);
+
     for (int c = 0; c < cols; ++c)
     {
       auto parameters = baseParams;
       parameters[xParamIndex] = xmin + (double)c / (cols - 1) * (xmax - xmin);
       parameters[yParamIndex] = ymax - (double)r / (rows - 1) * (ymax - ymin);
       const auto fval = f(parameters);
-      landscape[r][c] = fval;
-      landscapeLog[r][c] = std::log(fval);
+      landscape.at<float>(r, c) = fval;
+      landscapeLog.at<float>(r, c) = std::log(fval);
     }
   }
+
+  static const int landscapeSize = 1000;
+  static const double pointSizeMultiplier = 0.01;
+  static const double pointThicknessMultiplier = 0.005;
+  static const double lineThicknessMultiplier = 0.002;
+  static const double pointColorMultiplier = 0.8;
+  static const double pointColorMinMultiplier = 0.5;
+  resize(landscape, landscape, Size(landscapeSize, landscapeSize * rows / cols), 0, 0, INTER_LINEAR);
+  resize(landscapeLog, landscapeLog, Size(landscapeSize, landscapeSize * rows / cols), 0, 0, INTER_LINEAR);
+  rows = landscape.rows;
+  cols = landscape.cols;
+  double minVal, maxVal, minValLog, maxValLog;
+  Point minLoc;
+  minMaxLoc(landscape, &minVal, &maxVal, &minLoc);
+  minMaxLoc(landscapeLog, &minValLog, &maxValLog);
+  const int pointSize = pointSizeMultiplier * cols;
+  const int pointThickness = pointThicknessMultiplier * cols;
+  const int lineThickness = lineThicknessMultiplier * cols;
+  const Scalar pointColorMin(minVal + pointColorMinMultiplier * (maxVal - minVal));
+  const Scalar pointColorMinLog(minValLog + pointColorMinMultiplier * (maxValLog - minValLog));
+  const Scalar pointColor(minVal + pointColorMultiplier * (maxVal - minVal));
+  const Scalar pointColorLog(minValLog + pointColorMultiplier * (maxValLog - minValLog));
+  const Point pointOffset1(pointSize, pointSize);
+  const Point pointOffset2(pointSize, -pointSize);
+
+  if (optResult)
+  {
+    LOG_DEBUG("Drawing optimization progress ({} points)", optResult->parametersProgress.size());
+    for (int i = 0; i < optResult->parametersProgress.size(); i++)
+    {
+      double x = optResult->parametersProgress[i][xParamIndex];
+      double y = optResult->parametersProgress[i][yParamIndex];
+
+      Point point;
+      point.x = (x - xmin) / (xmax - xmin) * cols;
+      point.y = rows - 1 - (y - ymin) / (ymax - ymin) * rows;
+
+      line(landscape, point - pointOffset1, point + pointOffset1, pointColor, pointThickness);
+      line(landscape, point - pointOffset2, point + pointOffset2, pointColor, pointThickness);
+      line(landscapeLog, point - pointOffset1, point + pointOffset1, pointColorLog, pointThickness);
+      line(landscapeLog, point - pointOffset2, point + pointOffset2, pointColorLog, pointThickness);
+
+      if (i > 0)
+      {
+        double xprev = optResult->parametersProgress[i - 1][xParamIndex];
+        double yprev = optResult->parametersProgress[i - 1][yParamIndex];
+
+        Point pointprev;
+        pointprev.x = (xprev - xmin) / (xmax - xmin) * cols;
+        pointprev.y = rows - 1 - (yprev - ymin) / (ymax - ymin) * rows;
+
+        line(landscape, pointprev, point, pointColor, lineThickness);
+        line(landscapeLog, pointprev, point, pointColorLog, lineThickness);
+      }
+    }
+  }
+
+  line(landscape, minLoc - pointOffset1, minLoc + pointOffset1, pointColorMin, pointThickness);
+  line(landscape, minLoc - pointOffset2, minLoc + pointOffset2, pointColorMin, pointThickness);
+  line(landscapeLog, minLoc - pointOffset1, minLoc + pointOffset1, pointColorMinLog, pointThickness);
+  line(landscapeLog, minLoc - pointOffset2, minLoc + pointOffset2, pointColorMinLog, pointThickness);
 
   if (true)
   {
