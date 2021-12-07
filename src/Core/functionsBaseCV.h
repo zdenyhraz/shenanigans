b54c7ec75f36850b871d2c5d1119e56c71256c12
zdenyhraz <hrazdira.zdenek@gmail.com>
2021-12-07 19:43:56 +0100

    prec

--- src/Core/functionsBaseCV.h
+++ src/Core/functionsBaseCV.h
@@ -1,8 +1,5 @@
 #pragma once
 #include "opencv2/opencv.hpp"
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
 #include "Core/functionsBaseSTL.h"
 
 inline cv::Mat roicrop(const cv::Mat& sourceimgIn, int x, int y, int w, int h)
