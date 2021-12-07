3da9ccda1cc48fc3769ce92eb6dc0a13ce8b59fb
zdenyhraz <hrazdira.zdenek@gmail.com>
2021-12-07 18:21:26 +0100

    asd

--- src/Application/Application.cpp
+++ src/Application/Application.cpp
@@ -4,9 +4,8 @@ int main(int argc, char** argv)
 {
   srand(time(0));
 
-  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);  // DPI support
-  QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps); // HiDPI pixmaps
-
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
   QApplication a(argc, argv);
   WindowShenanigans w;
   w.show();
