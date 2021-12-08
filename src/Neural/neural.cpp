34847be04bf43147036884d55f5572d9e56aacf4
zdenyhraz <hrazdira.zdenek@gmail.com>
2021-12-08 16:30:42 +0100

    minus

--- src/Neural/neural.cpp
+++
@@ -1,7 +0,0 @@
-//artificial neural network functionality and features
-//updated frequently @ https://github.com/zdenyhraz
-//PhD work of Zdenek Hrazdira
-//made during 2019
-
-
-#include "neural.h"
