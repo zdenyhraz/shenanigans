b55fb3741fce197377ff379f22b5cb1f831705a2
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-01-01 16:52:51 +0100

    ret

--- src/Application/Application.cpp
+++ src/Application/Application.cpp
@@ -1,6 +1,6 @@
 #include "WindowShenanigans.h"
 
-i32 main(i32 argc, char** argv)
+int main(int argc, char** argv)
 try
 {
   srand(time(0));
@@ -14,8 +14,10 @@ try
 catch (const std::exception& e)
 {
   fmt::print("Error: {}\n", e.what());
+  return EXIT_FAILURE;
 }
 catch (...)
 {
   fmt::print("Error: {}\n", "An unknown error has occured");
+  return EXIT_FAILURE;
 }
