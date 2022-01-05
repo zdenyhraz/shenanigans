9a92e14f62c81738eca90635bffc8ea859223e10
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-01-05 13:08:33 +0100

    hehe

--- test/IterativePhaseCorrelationTest.cpp
+++ test/IterativePhaseCorrelationTest.cpp
@@ -3,54 +3,52 @@
 class IterativePhaseCorrelationTest : public ::testing::Test
 {
 protected:
-  IterativePhaseCorrelationTest() : mImg1(loadImage("../resources/Shapes/shape.png")), mImg2(loadImage("../resources/Shapes/shapes.png")) {}
+  IterativePhaseCorrelationTest()
+      : mImg1(loadImage("../resources/Shapes/shape.png")), mImg2(loadImage("../resources/Shapes/shapes.png")), mIPC(std::make_unique<IterativePhaseCorrelation>(mImg1.size()))
+  {
+  }
 
   const cv::Mat mImg1;
   const cv::Mat mImg2;
+  const std::unique_ptr<IterativePhaseCorrelation> mIPC;
 };
 
 TEST_F(IterativePhaseCorrelationTest, ZeroShiftPhaseCorrel)
 {
-  IterativePhaseCorrelation ipc(mImg1);
-  const auto shift = ipc.Calculate(mImg1, mImg1);
+  const auto shift = mIPC->Calculate(mImg1, mImg1);
   EXPECT_EQ(shift, cv::Point2f(0, 0));
 }
 
 TEST_F(IterativePhaseCorrelationTest, ZeroShiftCrossCorrel)
 {
-  IterativePhaseCorrelation ipc(mImg1);
-  const auto shift = ipc.Calculate<false, true>(mImg1, mImg1);
+  const auto shift = mIPC->Calculate<false, true>(mImg1, mImg1);
   EXPECT_EQ(shift, cv::Point2f(0, 0));
 }
 
 TEST_F(IterativePhaseCorrelationTest, ShiftPhaseCorrel)
 {
-  IterativePhaseCorrelation ipc(mImg1);
-  const auto shift = ipc.Calculate(mImg1, mImg2);
+  const auto shift = mIPC->Calculate(mImg1, mImg2);
   EXPECT_NEAR(shift.x, 97, 0.5);
   EXPECT_NEAR(shift.y, -104, 0.5);
 }
 
 TEST_F(IterativePhaseCorrelationTest, ShiftCrossCorrel)
 {
-  IterativePhaseCorrelation ipc(mImg1);
-  const auto shift = ipc.Calculate<false, true>(mImg1, mImg2);
+  const auto shift = mIPC->Calculate<false, true>(mImg1, mImg2);
   EXPECT_NEAR(shift.x, 97, 0.5);
   EXPECT_NEAR(shift.y, -104, 0.5);
 }
 
 TEST_F(IterativePhaseCorrelationTest, UnnormalizedInputsPhaseCorrel)
 {
-  IterativePhaseCorrelation ipc(mImg1);
-  const auto normShift = ipc.Calculate(mImg1, mImg2);
-  const auto unnormShift = ipc.Calculate(mImg1 * 25.73, mImg2 * 38.14);
+  const auto normShift = mIPC->Calculate(mImg1, mImg2);
+  const auto unnormShift = mIPC->Calculate(mImg1 * 25.73, mImg2 * 38.14);
   EXPECT_EQ(normShift, unnormShift);
 }
 
 TEST_F(IterativePhaseCorrelationTest, UnnormalizedInputsCrossCorrel)
 {
-  IterativePhaseCorrelation ipc(mImg1);
-  const auto normShift = ipc.Calculate<false, true>(mImg1, mImg2);
-  const auto unnormShift = ipc.Calculate<false, true>(mImg1 * 25.73, mImg2 * 38.14);
+  const auto normShift = mIPC->Calculate<false, true>(mImg1, mImg2);
+  const auto unnormShift = mIPC->Calculate<false, true>(mImg1 * 25.73, mImg2 * 38.14);
   EXPECT_EQ(normShift, unnormShift);
 }
