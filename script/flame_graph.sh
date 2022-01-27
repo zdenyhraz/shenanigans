6382b80d10c2b686912aeef1b7bec495cb1ffc90
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-01-27 03:05:54 +0100

    zzzz

---
+++ script/flame_graph.sh
@@ -0,0 +1,5 @@
+cd build
+perf record -a --call-graph fp ./shenanigans
+perf script > out.perf
+../external/flamegraph/stackcollapse-perf.pl out.perf > out.folded
+../external/flamegraph/flamegraph.pl out.folded > flamegraph.svg
