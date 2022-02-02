dd6ad9bb0eee7d727c292a58fb90e5bf86adc6b1
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-02-02 16:22:31 +0100

    cmnt

--- src/Astrophysics/DifferentialRotation.h
+++ src/Astrophysics/DifferentialRotation.h
@@ -155,8 +155,8 @@ public:
       f64 ret = 0;
       for (usize i = 0; i < omegasxfit.size(); ++i)
       {
-        ret += 0.75 * std::pow(omegasxfit[i] - predfit[i], 2); // pred fit diff
-        ret += 0.25 * std::pow(omegasx[i] - omegasxfit[i], 2); // variance
+        ret += 0.75 * std::pow(omegasxfit[i] - predfit[i], 2); // minimize pred fit diff
+        ret += 0.25 * std::pow(omegasx[i] - omegasxfit[i], 2); // minimize variance
       }
       return ret / omegasxfit.size();
     };
