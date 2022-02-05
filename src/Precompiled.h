e87ba4d951140f5ececb121773eb550418aa5adc
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-02-05 20:44:19 +0100

    address sanitizer in CI?

--- src/Precompiled.h
+++ src/Precompiled.h
@@ -88,13 +88,19 @@ namespace json = nlohmann;
 #ifdef ENABLE_PROFILING
   #ifdef TRACY_ENABLE
     #include <Tracy.hpp>
-    // use FrameMark for frames
+    // use FrameMark for frames (at the end of each frame)
+    // use ZoneScoped once per scope (automatic name)
+    // use ZoneScopedN once per scope (user-supplied name)
+    // use ZoneNamed for scopes inside ZoneScoped scope (user-supplied name)
+    // bool parameter of ZoneNamed can turn it on/off
     #define PROFILE_APP(name)
+    #define PROFILE_FRAME FrameMark
     #define PROFILE_FUNCTION ZoneScoped
-    #define PROFILE_SCOPE(name) ZoneNamedN(name, #name, true) // ZoneScoped/ZoneScopedN/ZoneNamedN
+    #define PROFILE_SCOPE(name) ZoneNamed(name, true)
   #else
     #include <optick.h>
     #define PROFILE_APP(name) OPTICK_APP(name)
+    #define PROFILE_FRAME
     #define PROFILE_FUNCTION OPTICK_EVENT()
     #define PROFILE_SCOPE(name) OPTICK_EVENT(name)
   #endif
