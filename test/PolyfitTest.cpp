7dc4cf9278dbe6da4dc72a53ca7407716a1177e0
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-02-12 15:29:18 +0100

    polyfit test

---
+++ test/PolyfitTest.cpp
@@ -0,0 +1,38 @@
+#include "Fit/Polyfit.h"
+
+TEST(PolyfitTest, Fit)
+{
+  i32 n = 101;
+  f64 c0 = 3.23, c1 = 2.56, c2 = 1.65;
+  std::vector<f64> x(n), y(n);
+
+  for (i32 i = 0; i < n; ++i)
+  {
+    x[i] = static_cast<f64>(i) / (n - 1);
+    y[i] = c0 + c1 * x[i] + c2 * x[i] * x[i];
+  }
+
+  const auto fy = polyfit(x, y, 2);
+  ASSERT_EQ(fy.size(), n);
+  for (i32 i = 0; i < n; ++i)
+    ASSERT_NEAR(fy[i], y[i], 1e-4);
+}
+
+TEST(PolyfitTest, Coeffs)
+{
+  i32 n = 101;
+  f64 c0 = 3.23, c1 = 2.56, c2 = 1.65;
+  std::vector<f64> x(n), y(n);
+
+  for (i32 i = 0; i < n; ++i)
+  {
+    x[i] = static_cast<f64>(i) / (n - 1);
+    y[i] = c0 + c1 * x[i] + c2 * x[i] * x[i];
+  }
+
+  const auto fc = polyfitCoeffs(x, y, 2);
+  ASSERT_EQ(fc.size(), 3);
+  ASSERT_NEAR(fc[0], c0, 1e-4);
+  ASSERT_NEAR(fc[1], c1, 1e-4);
+  ASSERT_NEAR(fc[2], c2, 1e-4);
+}
