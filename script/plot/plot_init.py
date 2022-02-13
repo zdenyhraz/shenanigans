65ca2b900558c5c90588ab8a056ec7f890af323b
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-02-13 23:36:32 +0100

    move init to a file

---
+++ script/plot/plot_init.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+FONT_SCALE = 2.2
+SMALL_SIZE = 8*FONT_SCALE
+MEDIUM_SIZE = 10*FONT_SCALE
+BIGGER_SIZE = 12*FONT_SCALE
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
