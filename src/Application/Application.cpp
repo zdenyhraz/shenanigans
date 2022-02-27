f4023d2ff41158a0da33b4fcb2d76ea7c30b58a9
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-02-27 15:56:54 +0100

    main

--- src/Application/Application.cpp
+++ src/Application/Application.cpp
@@ -1,20 +1,39 @@
 #include "Windows/Shenanigans/WindowShenanigans.h"
 
-int main(int argc, char** argv)
-try
+void RandInitialize()
 {
   std::srand(std::time(nullptr));
-  py::scoped_interpreter guard{};
-  PyPlot::Initialize();
+}
 
+void PythonInitialize()
+{
+  static py::scoped_interpreter guard{};
+}
+
+void PyPlotInitialize()
+{
+  PyPlot::Initialize();
+}
+
+void QtInitialize()
+{
   qputenv("QT_SCALE_FACTOR", "2.0");
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+}
 
-  QApplication a(argc, argv);
-  WindowShenanigans w;
-  w.show();
-  return a.exec();
+int main(int argc, char** argv)
+try
+{
+  RandInitialize();
+  PythonInitialize();
+  PyPlotInitialize();
+  QtInitialize();
+
+  QApplication application(argc, argv);
+  WindowShenanigans window;
+  window.show();
+  return application.exec();
 }
 catch (const std::exception& e)
 {
@@ -23,6 +42,6 @@ catch (const std::exception& e)
 }
 catch (...)
 {
-  fmt::print("Error: {}\n", "An unknown error has occured");
+  fmt::print("Error: Unknown error\n");
   return EXIT_FAILURE;
 }
