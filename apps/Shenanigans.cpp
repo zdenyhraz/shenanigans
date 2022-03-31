c9c9807fbd1d3c6143cbcdebf415288d49e66a51
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-03-31 23:51:10 +0200

    imgui

--- apps/Shenanigans.cpp
+++ apps/Shenanigans.cpp
@@ -1,7 +1,17 @@
-
 void PythonInitialize()
 {
   static py::scoped_interpreter guard{};
+}
+
+static void GLFWErrorCallback(int error, const char* description)
+{
+  fmt::print("GLFWError {}: {}\n", error, description);
+}
+
+static void GLFWKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
 int main(int argc, char** argv)
@@ -9,6 +19,93 @@ try
 {
   LOG_DEBUG("hi mom");
   PythonInitialize();
+
+  static constexpr float scale = 2;
+  glfwSetErrorCallback(GLFWErrorCallback);
+  if (not glfwInit())
+    throw std::runtime_error("GLFW initialization failed");
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  GLFWwindow* window = glfwCreateWindow(1920, 1080, "hi mom", NULL, NULL);
+  if (not window)
+    throw std::runtime_error("GLFW create window failed");
+  glfwMakeContextCurrent(window);
+  gladLoadGL();
+  glfwSwapInterval(1);
+  glfwSetKeyCallback(window, GLFWKeyCallback);
+
+  std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
+  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
+  io.FontGlobalScale = scale;
+  ImGui::StyleColorsDark();
+
+  ImGuiStyle& style = ImGui::GetStyle();
+  style.ScaleAllSizes(scale);
+  if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+  {
+    style.WindowRounding = 0.0f;
+    style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+  }
+
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 130");
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    {
+      ImGui::Begin("ddx");
+      ImGui::Button("Hi mom");
+      static float value = 0;
+      ImGui::DragFloat("value", &value);
+      ImGui::End();
+    }
+
+    {
+      ImGui::Begin("xxd");
+      ImGui::Button("Hi mom");
+      static float value = 0;
+      ImGui::DragFloat("value", &value);
+      ImGui::End();
+    }
+
+    ImGui::Render();
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+      GLFWwindow* backup_current_context = glfwGetCurrentContext();
+      ImGui::UpdatePlatformWindows();
+      ImGui::RenderPlatformWindowsDefault();
+      glfwMakeContextCurrent(backup_current_context);
+    }
+
+    glfwSwapBuffers(window);
+  }
+
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
   return EXIT_SUCCESS;
 }
 catch (const std::exception& e)
