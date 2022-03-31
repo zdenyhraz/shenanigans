e0980571e82a8328050cac23bb66f4c46f5cdb36
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-03-31 21:48:49 +0200

    xx

--- src/Application/Application.cpp
+++ src/Application/Application.cpp
@@ -1,7 +1,7 @@
 // #include <imgui.h>
 // #include <imgui_impl_glfw.h>
 // #include <imgui_impl_opengl3.h>
-#include <glad/glad.h>
+//#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include "Windows/Shenanigans/WindowShenanigans.hpp"
