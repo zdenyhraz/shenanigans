cbb88ffae394ece87aa5fd5a1105b7346230b87c
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-04-01 13:10:07 +0200

    diffrot optimize

--- apps/shenanigans/DiffrotWindow.cpp
+++ apps/shenanigans/DiffrotWindow.cpp
@@ -1,4 +1,6 @@
 #include "DiffrotWindow.hpp"
+#include "IPCWindow.hpp"
+#include "Astrophysics/DifferentialRotation.hpp"
 
 void DiffrotWindow::Initialize()
 {
@@ -7,5 +9,27 @@ void DiffrotWindow::Initialize()
 void DiffrotWindow::Render()
 {
   ImGui::Begin("Differential rotation");
+
+  if (ImGui::Button("Calculate"))
+    DifferentialRotation(mParameters.xsize, mParameters.ysize, mParameters.idstep, mParameters.idstride, mParameters.thetamax / Constants::Rad, mParameters.cadence)
+        .Calculate(IPCWindow::GetIPC(), mParameters.dataPath, mParameters.idstart);
+
+  ImGui::SliderInt("xsize", &mParameters.xsize, 1, 2500);
+  ImGui::SliderInt("ysize", &mParameters.ysize, 3, 301);
+  ImGui::SliderInt("id step", &mParameters.idstep, 1, 5);
+  ImGui::SliderInt("id stride", &mParameters.idstride, 1, 25);
+  ImGui::SliderFloat("thetamax", &mParameters.thetamax, 20, 80);
+  ImGui::SliderInt("cadence", &mParameters.cadence, 25, 100);
+  ImGui::InputText("Data path", &mParameters.dataPath);
+  ImGui::InputInt("id start", &mParameters.idstart);
+
+  if (ImGui::Button("Optimize"))
+    DifferentialRotation(mParameters.xsize, mParameters.ysize, mParameters.idstep, mParameters.idstride, mParameters.thetamax / Constants::Rad, mParameters.cadence)
+        .Optimize(IPCWindow::GetIPC(), mParameters.dataPath, mParameters.idstart, mParameters.xsizeopt, mParameters.ysizeopt, mParameters.popsize);
+
+  ImGui::SliderInt("xsizeopt", &mParameters.xsizeopt, 1, 500);
+  ImGui::SliderInt("ysizeopt", &mParameters.ysizeopt, 3, 201);
+  ImGui::SliderInt("popsize", &mParameters.popsize, 6, 36);
+
   ImGui::End();
 }
--- apps/shenanigans/DiffrotWindow.hpp
+++ apps/shenanigans/DiffrotWindow.hpp
@@ -1,5 +1,21 @@
 #pragma once
 
+struct DiffrotParameters
+{
+  i32 xsize = 2500;
+  i32 ysize = 101;
+  i32 idstep = 1;
+  i32 idstride = 25;
+  f32 thetamax = 50;
+  i32 cadence = 45;
+  std::string dataPath = "../data/diffrot_month_5000";
+  i32 idstart = 123132;
+
+  i32 xsizeopt = 1;
+  i32 ysizeopt = 101;
+  i32 popsize = 6;
+};
+
 class DiffrotWindow
 {
 public:
@@ -7,4 +23,5 @@ public:
   static void Render();
 
 private:
+  inline static DiffrotParameters mParameters;
 };
