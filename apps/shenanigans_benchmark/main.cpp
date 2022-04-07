7421b62738ed8beaaa3e2daacd8c5a3d5de60eae
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-04-07 22:59:01 +0200

    cv bench

--- apps/shenanigans_benchmark/main.cpp
+++ apps/shenanigans_benchmark/main.cpp
@@ -2,26 +2,41 @@
 
 using Input = std::vector<cv::Mat>;
 
-void OpenCVFFT(const Input& images)
+void OpenCVFFTComplex(const Input& images)
 {
+  cv::Mat fft;
   for (const auto& image : images)
-    const auto fft = Fourier::fft(image.clone());
+    cv::dft(image, fft, cv::DFT_COMPLEX_OUTPUT);
 }
 
-static void OpenCVFFTBenchmark(benchmark::State& state, const Input& images)
+void OpenCVFFTCSS(const Input& images)
+{
+  cv::Mat fft;
+  for (const auto& image : images)
+    cv::dft(image, fft);
+}
+
+static void OpenCVFFTComplexBenchmark(benchmark::State& state, const Input& images)
 {
   for (auto _ : state)
-  {
-    OpenCVFFT(images);
-  }
+    OpenCVFFTComplex(images);
+}
+
+static void OpenCVFFTCSSBenchmark(benchmark::State& state, const Input& images)
+{
+  for (auto _ : state)
+    OpenCVFFTCSS(images);
 }
 
 int main(int argc, char** argv)
 try
 {
+  static constexpr usize iterations = 100;
+  static constexpr benchmark::TimeUnit timeunit = benchmark::kMicrosecond;
   const auto images = LoadImages<f64>("../debug/ipcopt/train");
 
-  benchmark::RegisterBenchmark("OpenCVFFT", OpenCVFFTBenchmark, images);
+  benchmark::RegisterBenchmark("OpenCV Complex", OpenCVFFTComplexBenchmark, images)->Iterations(iterations)->Unit(timeunit);
+  benchmark::RegisterBenchmark("OpenCV CSS", OpenCVFFTCSSBenchmark, images)->Iterations(iterations)->Unit(timeunit);
 
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
