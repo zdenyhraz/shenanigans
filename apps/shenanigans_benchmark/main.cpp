71ebcbf7ad0f4ec9efdaeaa3152b58821bcf5318
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-04-11 13:39:00 +0200

    fftw vector

--- apps/shenanigans_benchmark/main.cpp
+++ apps/shenanigans_benchmark/main.cpp
@@ -1,31 +1,66 @@
 #include "Fourier/Fourier.hpp"
 #include <fftw3.h>
 
-// state.PauseTiming();
-// state.ResumeTiming();
-
-using Precision = f32;
-
-std::vector<Precision> GenerateRandomVector(usize size)
+namespace fftw
 {
-  std::vector<Precision> vec(size);
+template <typename T>
+class vector
+{
+  T* mData;
+  usize mSize;
+
+public:
+  vector(usize n)
+  {
+    if constexpr (std::is_same_v<T, f32>)
+      mData = fftwf_alloc_real(n);
+    if constexpr (std::is_same_v<T, fftwf_complex>)
+      mData = fftwf_alloc_complex(n);
+
+    if constexpr (std::is_same_v<T, f64>)
+      mData = fftw_alloc_real(n);
+    if constexpr (std::is_same_v<T, fftw_complex>)
+      mData = fftw_alloc_complex(n);
+
+    mSize = n;
+  }
+
+  ~vector()
+  {
+    if constexpr (std::is_same_v<T, f32> or std::is_same_v<T, fftwf_complex>)
+      fftwf_free(mData);
+    if constexpr (std::is_same_v<T, f64> or std::is_same_v<T, fftw_complex>)
+      fftw_free(mData);
+  }
+
+  vector(const vector& other) = delete;
+
+  T* data() const { return mData; }
+
+  usize size() const { return mSize; }
+};
+}
+
+std::vector<f32> GenerateRandomVector(usize size)
+{
+  std::vector<f32> vec(size);
   for (auto& x : vec)
     x = Random::Rand(0, 1);
   return vec;
 }
 
-static void OpenCVBenchmark(benchmark::State& state, std::vector<Precision> input, std::vector<Precision> output)
+static void OpenCVBenchmark(benchmark::State& state, std::vector<f32> input)
 {
-  cv::Mat inputWrapper(1, input.size(), GetMatType<Precision>(1), input.data());
-  cv::Mat outputWrapper(1, input.size(), GetMatType<Precision>(2), output.data());
+  cv::Mat inputWrapper(1, input.size(), GetMatType<f32>(1), input.data());
+  cv::Mat output(1, input.size(), GetMatType<f32>(2));
 
   for (auto _ : state)
   {
-    cv::dft(inputWrapper, outputWrapper, cv::DFT_COMPLEX_OUTPUT);
+    cv::dft(inputWrapper, output, cv::DFT_COMPLEX_OUTPUT);
   }
 }
 
-static void FFTWBenchmark(benchmark::State& state, std::vector<Precision> input, std::vector<Precision> output)
+static void FFTWBenchmark(benchmark::State& state, std::vector<f32> input)
 {
   // from https://www.fftw.org/fftw3_doc/One_002dDimensional-DFTs-of-Real-Data.html:
   // In many practical applications, the input data in[i] are purely real numbers, in which case the DFT output satisfies the “Hermitian” redundancy: out[i] is the conjugate of out[n-i]. It is
@@ -33,7 +68,11 @@ static void FFTWBenchmark(benchmark::State& state, std::vector<Precision> input,
   // user sacrifices some of the simplicity of FFTW’s complex transforms. First of all, the input and output arrays are of different sizes and types: the input is n real numbers, while the output is
   // n/2+1 complex numbers (the non-redundant outputs); this also requires slight “padding” of the input array for in-place transforms. Second, the inverse transform (complex to real) has the
   // side-effect of overwriting its input array, by default. Neither of these inconveniences should pose a serious problem for users, but it is important to be aware of them.
-  fftwf_plan plan = fftwf_plan_dft_r2c_1d(input.size(), input.data(), reinterpret_cast<fftwf_complex*>(output.data()), FFTW_ESTIMATE);
+
+  fftw::vector<f32> inputAligned(input.size());
+  fftw::vector<fftwf_complex> outputAligned(input.size() / 2 + 1);
+  std::memcpy(inputAligned.data(), input.data(), input.size());
+  fftwf_plan plan = fftwf_plan_dft_r2c_1d(input.size(), inputAligned.data(), outputAligned.data(), FFTW_ESTIMATE);
   for (auto _ : state)
   {
     fftwf_execute(plan);
@@ -53,11 +92,10 @@ try
   for (const auto exponent : exponents)
   {
     const auto size = 1 << exponent;
-    const auto input = GenerateRandomVector(size);      // real
-    const auto output = GenerateRandomVector(size * 2); // complex
+    const auto input = GenerateRandomVector(size); // real
 
-    benchmark::RegisterBenchmark(fmt::format("{:>8} | OpenCV", size).c_str(), OpenCVBenchmark, input, output)->Unit(timeunit);
-    benchmark::RegisterBenchmark(fmt::format("{:>8} | FFTW", size).c_str(), FFTWBenchmark, input, output)->Unit(timeunit);
+    benchmark::RegisterBenchmark(fmt::format("{:>8} | OpenCV", size).c_str(), OpenCVBenchmark, input)->Unit(timeunit);
+    benchmark::RegisterBenchmark(fmt::format("{:>8} | FFTW", size).c_str(), FFTWBenchmark, input)->Unit(timeunit);
   }
 
   benchmark::Initialize(&argc, argv);
