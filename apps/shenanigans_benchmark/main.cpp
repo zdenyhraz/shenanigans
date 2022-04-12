7863cd82c85ae228556b67cc368b4e2d3d8c5706
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-04-12 14:10:59 +0200

    - fftw

--- apps/shenanigans_benchmark/main.cpp
+++ apps/shenanigans_benchmark/main.cpp
@@ -1,6 +1,3 @@
-#include <fftw3.h>
-#include "fftw_vector.hpp"
-
 std::vector<f32> GenerateRandomVector(usize size)
 {
   std::vector<f32> vec(size);
@@ -8,59 +5,22 @@ std::vector<f32> GenerateRandomVector(usize size)
   return vec;
 }
 
-static void OpenCVBenchmark(benchmark::State& state, std::vector<f32> input)
+static void OpenCVCCSBenchmark(benchmark::State& state, std::vector<f32> input)
 {
   std::vector<f32> output(input.size() / 2 + 1);
   for (auto _ : state)
   {
-    cv::dft(input, output, cv::DFT_ROWS);
+    cv::dft(input, output);
   }
 }
 
-static void FFTWEstimateBenchmark(benchmark::State& state, std::vector<f32> input)
+static void OpenCVComplexBenchmark(benchmark::State& state, std::vector<f32> input)
 {
-  // from https://www.fftw.org/fftw3_doc/One_002dDimensional-DFTs-of-Real-Data.html
-  // In many practical applications, the input data in[i] are purely real numbers, in which case the DFT output satisfies the “Hermitian” redundancy:
-  // out[i] is the conjugate of out[n-i]. It is possible to take advantage of these circumstances in order to achieve roughly a factor of two
-  // improvement in both speed and memory usage. In exchange for these speed and space advantages, the user sacrifices some of the simplicity of
-  // FFTW’s complex transforms. First of all, the input and output arrays are of different sizes and types: the input is n real numbers, while the
-  // output is n/2+1 complex numbers (the non-redundant outputs); this also requires slight “padding” of the input array for in-place transforms.
-  // Second, the inverse transform (complex to real) has the side-effect of overwriting its input array, by default. Neither of these inconveniences
-  // should pose a serious problem for users, but it is important to be aware of them.
-
-  fftw::vector<f32> inputAligned(input.size());
-  fftw::vector<fftwf_complex> outputAligned(input.size() / 2 + 1);
-  std::memcpy(inputAligned.data(), input.data(), input.size());
-  fftwf_plan plan = fftwf_plan_dft_r2c_1d(input.size(), inputAligned.data(), outputAligned.data(), FFTW_ESTIMATE);
+  std::vector<std::complex<f32>> output(input.size());
   for (auto _ : state)
   {
-    fftwf_execute(plan);
+    cv::dft(input, output, cv::DFT_COMPLEX_OUTPUT);
   }
-  fftwf_destroy_plan(plan);
-  fftwf_cleanup();
-}
-
-static void FFTWMeasureBenchmark(benchmark::State& state, std::vector<f32> input)
-{
-  // from https://www.fftw.org/fftw3_doc/One_002dDimensional-DFTs-of-Real-Data.html
-  // In many practical applications, the input data in[i] are purely real numbers, in which case the DFT output satisfies the “Hermitian” redundancy:
-  // out[i] is the conjugate of out[n-i]. It is possible to take advantage of these circumstances in order to achieve roughly a factor of two
-  // improvement in both speed and memory usage. In exchange for these speed and space advantages, the user sacrifices some of the simplicity of
-  // FFTW’s complex transforms. First of all, the input and output arrays are of different sizes and types: the input is n real numbers, while the
-  // output is n/2+1 complex numbers (the non-redundant outputs); this also requires slight “padding” of the input array for in-place transforms.
-  // Second, the inverse transform (complex to real) has the side-effect of overwriting its input array, by default. Neither of these inconveniences
-  // should pose a serious problem for users, but it is important to be aware of them.
-
-  fftw::vector<f32> inputAligned(input.size());
-  fftw::vector<fftwf_complex> outputAligned(input.size() / 2 + 1);
-  std::memcpy(inputAligned.data(), input.data(), input.size());
-  fftwf_plan plan = fftwf_plan_dft_r2c_1d(input.size(), inputAligned.data(), outputAligned.data(), FFTW_MEASURE);
-  for (auto _ : state)
-  {
-    fftwf_execute(plan);
-  }
-  fftwf_destroy_plan(plan);
-  fftwf_cleanup();
 }
 
 // --benchmark_out_format={json|console|csv}
@@ -78,9 +38,8 @@ try
     const auto size = 1 << exponent;
     const auto input = GenerateRandomVector(size); // real
 
-    benchmark::RegisterBenchmark(fmt::format("{:>8} | OpenCV-IPP ccs", size).c_str(), OpenCVBenchmark, input)->Unit(timeunit);
-    benchmark::RegisterBenchmark(fmt::format("{:>8} | FFTW est r2c", size).c_str(), FFTWEstimateBenchmark, input)->Unit(timeunit);
-    benchmark::RegisterBenchmark(fmt::format("{:>8} | FFTW mes r2c", size).c_str(), FFTWMeasureBenchmark, input)->Unit(timeunit);
+    benchmark::RegisterBenchmark(fmt::format("{:>8} | OpenCV cplx", size).c_str(), OpenCVComplexBenchmark, input)->Unit(timeunit);
+    benchmark::RegisterBenchmark(fmt::format("{:>8} | OpenCV ccs", size).c_str(), OpenCVCCSBenchmark, input)->Unit(timeunit);
   }
 
   benchmark::Initialize(&argc, argv);
