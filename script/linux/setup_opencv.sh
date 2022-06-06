ad90b1da4def54352fa5f9b4cbcf1158d939ee75
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-06-06 21:41:49 +0200

    opencv b

--- script/linux/setup_opencv.sh
+++ script/linux/setup_opencv.sh
@@ -5,6 +5,6 @@ sudo chmod 755 4.5.5.tar.gz
 tar -xf 4.5.5.tar.gz && mv opencv-4.5.5 opencv && cd opencv
 git clone https://github.com/opencv/opencv_contrib.git
 mkdir build && cd build
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DOPENCV_ENABLE_NONFREE=ON -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules -DBUILD_opencv_java=OFF
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DOPENCV_ENABLE_NONFREE=ON -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules -DBUILD_opencv_python=OFF -DBUILD_opencv_java=OFF
 cmake --build . --config Release -j 6
 sudo ninja install
