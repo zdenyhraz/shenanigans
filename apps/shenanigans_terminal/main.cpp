6b43d2f29c96fb9966429de09d79138e1feca7c0
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-09-20 16:46:51 +0200

    lowpass

--- apps/shenanigans_terminal/main.cpp
+++ apps/shenanigans_terminal/main.cpp
@@ -7,12 +7,12 @@ try
 
   PyPlot::SetSave(true);
 
-  const auto image1Path = "/home/hrazdira/shenanigans/debug/tfs_2A.tif";
-  const auto image2Path = "/home/hrazdira/shenanigans/debug/tfs_2B.tif";
+  const auto image1Path = "/home/hrazdira/shenanigans/debug/tfs_1A.tif";
+  const auto image2Path = "/home/hrazdira/shenanigans/debug/tfs_1B.tif";
   const f64 noiseStddev = 0;
   const auto image = LoadUnitFloatImage<IPC::Float>(image1Path);
   const auto size = std::min(image.rows, image.cols);
-  const auto ipc = IPC(size, size);
+  const auto ipc = IPC(size, size, 0, 0.5);
   IPCDebug::DebugAlign(ipc, image1Path, image2Path, noiseStddev);
 
   return EXIT_SUCCESS;
