145e733c7574a2454ff8e6d4fd82d3d20f121fd9
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-12-05 14:16:55 +0100

    install

--- script/windows/setup_opencv.bat
+++ script/windows/setup_opencv.bat
@@ -3,5 +3,4 @@ git clone https://github.com/opencv/opencv_contrib.git --branch 4.x --single-bra
 cd opencv && mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DOPENCV_ENABLE_NONFREE=ON -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules -DBUILD_TESTS=OFF -DBUILD_opencv_python=OFF -DBUILD_opencv_java=OFF -DBUILD_opencv_apps=OFF
 cmake --build . --config Release
-cmake install
-
+cmake --install . --prefix "../install"
