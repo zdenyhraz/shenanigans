d8a7349537e68999c3f9bc59769a1a8f19a98718
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-12-05 20:14:33 +0100

    kkk

--- script/windows/setup_torch.bat
+++ script/windows/setup_torch.bat
@@ -2,3 +2,5 @@ cd libs
 wget https://download.pytorch.org/libtorch/cpu/libtorch-win-shared-with-deps-1.13.0%2Bcpu.zip -O libtorch.zip
 tar -xf libtorch.zip
 del libtorch.zip
+
+# delete lazy_init_num_threads() function definition @libs\libtorch\include\ATen\Parallel.h
