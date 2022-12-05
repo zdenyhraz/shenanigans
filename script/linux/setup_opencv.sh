8e7385d7876c08506a3a4dd3251cce215ad67026
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-12-05 14:39:58 +0100

    scriptx

--- script/linux/setup_opencv.sh
+++ script/linux/setup_opencv.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-wget https://github.com/opencv/opencv/archive/refs/tags/4.5.5.tar.gz
-sudo chmod 755 4.5.5.tar.gz
-tar -xf 4.5.5.tar.gz && mv opencv-4.5.5 opencv && cd opencv
-git clone https://github.com/opencv/opencv_contrib.git
-mkdir build && cd build
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DOPENCV_ENABLE_NONFREE=ON -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules -DBUILD_TESTS=OFF -DBUILD_opencv_python=OFF -DBUILD_opencv_java=OFF -DBUILD_opencv_apps=OFF
+git clone https://github.com/opencv/opencv.git --branch 4.x --single-branch --depth 1
+git clone https://github.com/opencv/opencv_contrib.git --branch 4.x --single-branch --depth 1
+cd opencv && mkdir build && cd build
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DOPENCV_ENABLE_NONFREE=ON -DOPENCV_EXTRA_MODULES_PATH="../../opencv_contrib/modules" -DBUILD_TESTS=OFF -DBUILD_opencv_python=OFF -DBUILD_opencv_java=OFF -DBUILD_opencv_apps=OFF
 cmake --build . --config Release
 sudo ninja install
---
+++ script/windows/setup_torch.bat
@@ -0,0 +1,4 @@
+cd libs
+wget https://download.pytorch.org/libtorch/cpu/libtorch-win-shared-with-deps-1.13.0%2Bcpu.zip -O libtorch.zip
+tar -xf libtorch.zip
+del libtorch.zip
