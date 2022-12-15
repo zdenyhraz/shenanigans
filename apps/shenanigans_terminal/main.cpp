ce7698b06b407575c80ee95e044a199e3426121c
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-12-15 17:03:53 +0100

    xxx

--- apps/shenanigans_terminal/main.cpp
+++ apps/shenanigans_terminal/main.cpp
@@ -7,23 +7,6 @@ void RunObjectDetection()
   auto image = LoadUnitFloatImage<f64>("../data/debug/ObjectDetection/rocks1.png");
   cv::normalize(image, image, 0, 255, cv::NORM_MINMAX);
   image.convertTo(image, CV_8U);
-  const auto objectSize = image.cols / 50; // 50
-
-  {
-    const auto objectThreshold = 0.01; // 0.15
-    const auto blurSize = 11;
-    const auto stddevSize = 9;
-    DetectObjectsStddev(image, objectSize, objectThreshold, blurSize, stddevSize);
-  }
-
-  {
-    const auto objectThreshold = 0.01; // 0.03
-    const auto blurSize = 11;
-    const auto sobelSize = 3;
-    const auto lowThreshold = 0.3;
-    const auto highThreshold = 3 * lowThreshold;
-    DetectObjectsCanny(image, objectSize, objectThreshold, blurSize, sobelSize, lowThreshold, highThreshold);
-  }
 }
 
 void RunUnevenIlluminationCLAHE()
@@ -47,7 +30,6 @@ int main(int argc, char** argv)
 try
 {
   LOG_FUNCTION;
-  // RunObjectDetection();
   // RunUnevenIlluminationCLAHE();
   RunUnevenIlluminationHomomorphic();
 
