a96245a6489f55bd60aded40cd9c11ae773ed3b5
zdenyhraz <hrazdira.zdenek@gmail.com>
2023-01-19 18:54:55 +0100

    okok

--- script/xtf/xtf_visual.py
+++ script/xtf/xtf_visual.py
@@ -3,10 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-def normalize(vec):
-    norm=np.linalg.norm(vec)
-    return vec/norm
-
 if True:
   file = r'M:\Work\shenanigans\data\debug\ObjectDetection\xtf\sasi-S-upper-20221102-144815-l38.xtf'
 
@@ -20,10 +16,10 @@ if True:
   image.clip(0, upper_limit-1, out=image) # Clip to range (max cannot be used due to outliers) More robust methods are possible (through histograms / statistical outlier removal)
   image = np.log10(image + 0.0001) # The sonar data is logarithmic (dB), add small value to avoid log10(0)
 
-  fig, (ax1, ax2) = plt.subplots(1,2, figsize=(12,8))
+  fig, (ax1, ax2,ax3) = plt.subplots(1,3, figsize=(30,10))
   fig.canvas.manager.set_window_title(file)
   row_sums = image.sum(axis=1)
-  image = image / row_sums[:, np.newaxis]
+  image_normalized = image / row_sums[:, np.newaxis]
 
   # row_sums = image.sum(axis=0)
   # image = image / row_sums[np.newaxis,:]
@@ -31,6 +27,7 @@ if True:
   # plt.imshow(image, cmap='gray', vmin=0, vmax=np.log10(upper_limit), aspect='auto') # waterfall-view
   ax1.imshow(image, cmap='gray', aspect='auto') # waterfall-view
   ax2.plot(row_sums[::-1],np.linspace(0,row_sums.shape[0]-1,num=row_sums.shape[0]))
+  ax3.imshow(image_normalized, cmap='gray', aspect='auto') # waterfall-view
 
   plt.tight_layout()
   plt.show()
