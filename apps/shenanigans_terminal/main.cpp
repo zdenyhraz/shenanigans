f454bdbf493257c8301b145a10b1637b2e0c5b41
zdenyhraz <hrazdira.zdenek@gmail.com>
2023-01-20 14:11:21 +0100

    idk

--- apps/shenanigans_terminal/main.cpp
+++ apps/shenanigans_terminal/main.cpp
@@ -1,37 +1,8 @@
-#include "Random/ObjectDetection.hpp"
-#include "Random/UnevenIllumination.hpp"
-
-void RunObjectDetection()
-{
-  LOG_FUNCTION;
-  auto image = LoadUnitFloatImage<f64>("../data/debug/ObjectDetection/rocks1.png");
-  cv::normalize(image, image, 0, 255, cv::NORM_MINMAX);
-  image.convertTo(image, CV_8U);
-}
-
-void RunUnevenIlluminationCLAHE()
-{
-  LOG_FUNCTION;
-  auto image = cv::imread("../data/debug/UnevenIllumination/input.jpg");
-  const auto tileGridSize = 8;
-  const auto clipLimit = 1;
-  CorrectUnevenIlluminationCLAHE(image, tileGridSize, clipLimit);
-}
-
-void RunUnevenIlluminationHomomorphic()
-{
-  LOG_FUNCTION;
-  auto image = cv::imread("../data/debug/UnevenIllumination/input.jpg");
-  for (auto cutoff = 0.001; cutoff <= 0.02; cutoff += 0.001)
-    CorrectUnevenIlluminationHomomorphic(image, cutoff);
-}
 
 int main(int argc, char** argv)
 try
 {
   LOG_FUNCTION;
-  // RunUnevenIlluminationCLAHE();
-  RunUnevenIlluminationHomomorphic();
 
   return EXIT_SUCCESS;
 }
