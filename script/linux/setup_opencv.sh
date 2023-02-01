82ab78243e5853bcb9ca6ac7a6fdbca19919be9b
zdenyhraz <hrazdira.zdenek@gmail.com>
2023-02-01 18:57:15 +0100

    gogog

--- script/linux/setup_opencv.sh
+++ script/linux/setup_opencv.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 git clone https://github.com/opencv/opencv.git --branch 4.x --single-branch --depth 1
+cd opencv
 git clone https://github.com/opencv/opencv_contrib.git --branch 4.x --single-branch --depth 1
-cd opencv && mkdir build && cd build
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DOPENCV_ENABLE_NONFREE=ON -DOPENCV_EXTRA_MODULES_PATH="../../opencv_contrib/modules" -DBUILD_TESTS=OFF -DBUILD_opencv_python=OFF -DBUILD_opencv_java=OFF -DBUILD_opencv_apps=OFF
+mkdir build && cd build
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DOPENCV_ENABLE_NONFREE=ON -DOPENCV_EXTRA_MODULES_PATH="../opencv_contrib/modules" -DBUILD_TESTS=OFF -DBUILD_opencv_python=OFF -DBUILD_opencv_java=OFF -DBUILD_opencv_apps=OFF
 cmake --build . --config Release
 sudo ninja install
