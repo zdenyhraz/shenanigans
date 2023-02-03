2ca0ba8255eac75c967f38d3196e91a230245be5
zdenyhraz <hrazdira.zdenek@gmail.com>
2023-02-03 11:23:46 +0100

    jaja oki

--- src/Gui/Application.cpp
+++ src/Gui/Application.cpp
@@ -101,5 +101,5 @@ void Application::SetWindowIcon(GLFWwindow* window)
 void Application::KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, GLFW_TRUE);
+    GLFWCloseWindow(window);
 }
