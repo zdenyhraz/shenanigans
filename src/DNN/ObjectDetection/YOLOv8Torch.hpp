516672e7228ddb47df80d1f4d84c83f3c153e7ca
zdenyhraz <hrazdira.zdenek@gmail.com>
2023-02-23 03:33:57 +0100

    refacc

--- src/DNN/ObjectDetection/YOLOv8Torch.hpp
+++ src/DNN/ObjectDetection/YOLOv8Torch.hpp
@@ -3,6 +3,7 @@
 
 std::vector<std::string> LoadClassNames(const std::filesystem::path& classesPath)
 {
+  LOG_FUNCTION;
   std::vector<std::string> classes;
   std::ifstream inputFile(classesPath);
   if (inputFile.is_open())
@@ -15,60 +16,57 @@ std::vector<std::string> LoadClassNames(const std::filesystem::path& classesPath
   return classes;
 }
 
-void DetectObjectsYOLOv8Torch(const cv::Mat& source, const std::filesystem::path& modelPath, const std::filesystem::path& classesPath, f32 confidenceThreshold, f32 NMSThreshold)
+std::vector<torch::jit::IValue> Preprocess(const cv::Mat& source, cv::Size modelShape)
 {
   LOG_FUNCTION;
-  const auto modelShape = cv::Size(640, 640);
-  const auto classes = LoadClassNames(classesPath);
-
-  cv::Mat image = source.clone();
-  cv::Mat blob;
-  cv::dnn::blobFromImage(image, blob, 1.0 / 255.0, modelShape, cv::Scalar(), true);
+  static cv::Mat blob;
+  cv::dnn::blobFromImage(source, blob, 1.0 / 255.0, modelShape, cv::Scalar(), true);
   torch::Tensor input = torch::from_blob(blob.data, {1, 3, modelShape.width, modelShape.height});
-  f32 scaleX = static_cast<f32>(image.cols) / modelShape.width;
-  f32 scaleY = static_cast<f32>(image.rows) / modelShape.height;
-
   LOG_DEBUG("Input tensor size: [{},{},{},{}]", input.sizes()[0], input.sizes()[1], input.sizes()[2], input.sizes()[3]);
   std::vector<torch::jit::IValue> inputs;
   inputs.push_back(input);
+  return inputs;
+}
 
-  torch::jit::script::Module module;
-  {
-    LOG_SCOPE("Load network");
-    module = torch::jit::load(modelPath.string());
-  }
-  torch::jit::IValue output;
-  {
-    LOG_SCOPE("Forward");
-    output = module.forward(inputs);
-  }
+torch::jit::script::Module LoadModel(const std::filesystem::path& modelPath)
+{
+  LOG_FUNCTION;
+  return torch::jit::load(modelPath.string());
+}
 
-  torch::Tensor outputTensor = output.toTensor();
-  LOG_DEBUG("Raw output tensor size: [{},{},{},{}]", outputTensor.sizes()[0], outputTensor.sizes()[1], outputTensor.sizes()[2], outputTensor.sizes()[3]);
-  outputTensor = outputTensor[0].transpose(0, 1); // ignore batch dim + transpose object & box+scores dim
-  LOG_DEBUG("Processed output tensor size: [{},{},{},{}]", outputTensor.sizes()[0], outputTensor.sizes()[1], outputTensor.sizes()[2], outputTensor.sizes()[3]);
-  const i64 objectCount = outputTensor.sizes()[0];
+torch::Tensor Forward(torch::jit::script::Module& model, std::vector<torch::jit::IValue>& inputs)
+{
+  LOG_FUNCTION;
+  return model.forward(inputs).toTensor();
+}
+
+cv::Mat Postprocess(torch::Tensor& output, f32 confidenceThreshold, f32 NMSThreshold, const std::vector<std::string>& classes, cv::Point2f imageScale, const cv::Mat source)
+{
+  LOG_FUNCTION;
+  LOG_DEBUG("Raw output tensor size: [{},{},{},{}]", output.sizes()[0], output.sizes()[1], output.sizes()[2], output.sizes()[3]);
+  output = output[0].transpose(0, 1); // ignore batch dim + transpose object & box+scores dim
+  LOG_DEBUG("Processed output tensor size: [{},{},{},{}]", output.sizes()[0], output.sizes()[1], output.sizes()[2], output.sizes()[3]);
   std::vector<i32> classids;
   std::vector<f32> confidences;
   std::vector<cv::Rect> boxes;
+  const i64 objectCount = output.sizes()[0];
   for (i64 i = 0; i < objectCount; ++i)
   {
-    const usize classid = torch::argmax(outputTensor[i].slice(0, 4)).item<f32>(); // box[x,y,w,h] + class scores
-    const f32 confidence = outputTensor[i].slice(0, 4)[classid].item<f32>();
+    const usize classid = torch::argmax(output[i].slice(0, 4)).item<f32>(); // box[x,y,w,h] + class scores
+    const f32 confidence = output[i].slice(0, 4)[classid].item<f32>();
 
     if (confidence < confidenceThreshold)
       continue;
 
-    const auto x = outputTensor[i][0].item<f32>();
-    const auto y = outputTensor[i][1].item<f32>();
-    const auto w = outputTensor[i][2].item<f32>();
-    const auto h = outputTensor[i][3].item<f32>();
-    const std::string className = classes[classid];
+    const auto x = output[i][0].item<f32>();
+    const auto y = output[i][1].item<f32>();
+    const auto w = output[i][2].item<f32>();
+    const auto h = output[i][3].item<f32>();
 
-    i32 left = (x - 0.5 * w) * scaleX;
-    i32 top = (y - 0.5 * h) * scaleY;
-    i32 width = w * scaleX;
-    i32 height = h * scaleY;
+    i32 left = (x - 0.5 * w) * imageScale.x;
+    i32 top = (y - 0.5 * h) * imageScale.y;
+    i32 width = w * imageScale.x;
+    i32 height = h * imageScale.y;
 
     classids.push_back(classid);
     confidences.push_back(confidence);
@@ -78,8 +76,22 @@ void DetectObjectsYOLOv8Torch(const cv::Mat& source, const std::filesystem::path
   std::vector<i32> boxesIdx;
   cv::dnn::NMSBoxes(boxes, confidences, confidenceThreshold, NMSThreshold, boxesIdx);
 
+  auto image = source.clone();
   for (const auto idx : boxesIdx)
     DrawPrediction(image, boxes[idx], classes[classids[idx]], confidences[idx]);
+  return image;
+}
+
+void DetectObjectsYOLOv8Torch(const cv::Mat& source, const std::filesystem::path& modelPath, const std::filesystem::path& classesPath, f32 confidenceThreshold, f32 NMSThreshold)
+{
+  LOG_FUNCTION;
+  const auto modelShape = cv::Size(640, 640);
+  const auto imageScale = cv::Point2f(static_cast<f32>(source.cols) / modelShape.width, static_cast<f32>(source.rows) / modelShape.height);
+  const auto classes = LoadClassNames(classesPath);
+  auto model = LoadModel(modelPath);
+  auto inputs = Preprocess(source, modelShape);
+  auto output = Forward(model, inputs);
+  const auto image = Postprocess(output, confidenceThreshold, NMSThreshold, classes, imageScale, source);
 
   Plot::Plot("YOLOv8Torch objects", image);
 }
