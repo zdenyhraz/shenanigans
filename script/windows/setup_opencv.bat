3e043dab1bffed5d94ed5067fcb168186e6d783c
zdenyhraz <hrazdira.zdenek@gmail.com>
2023-03-13 12:41:31 +0100

    idk

--- script/windows/setup_opencv.bat
+++ script/windows/setup_opencv.bat
@@ -7,6 +7,7 @@ cmake --build . --config Debug -j 8
 cmake --install . --prefix "../install/Debug"
 
 cmake .. -DCMAKE_BUILD_TYPE=Release -DOPENCV_ENABLE_NONFREE=ON -DOPENCV_EXTRA_MODULES_PATH="../../opencv_contrib/modules" -DBUILD_TESTS=OFF -DBUILD_opencv_python=OFF -DBUILD_opencv_java=OFF -DBUILD_opencv_apps=OFF
-:: cmake .. -DCMAKE_BUILD_TYPE=Release -DOPENCV_ENABLE_NONFREE=ON -DOPENCV_EXTRA_MODULES_PATH="../../opencv_contrib/modules" -DBUILD_TESTS=OFF -DBUILD_opencv_java=OFF -DBUILD_opencv_apps=OFF -DENABLE_FAST_MATH=ON -DWITH_CUDA=ON -DCUDA_FAST_MATH=ON -DCUDA_ARCH_BIN="8.9"
 cmake --build . --config Release -j 8
 cmake --install . --prefix "../install/Release"
+
+:: -DENABLE_FAST_MATH=ON -DWITH_CUDA=ON -DCUDA_FAST_MATH=ON -DCUDA_ARCH_BIN="8.9"
