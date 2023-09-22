d514f2718b86ec9c5275437b92f31ed43316e35a
zdenyhraz <hrazdira.zdenek@gmail.com>
2023-09-22 15:57:26 +0200

    cuda working

--- script/dnn/image_classification.py
+++ script/dnn/image_classification.py
@@ -5,7 +5,8 @@ import torch.optim as optim
 import torch.utils.data
 import os.path
 from pathlib import Path
-from skimage import io
+import cv2
+import skimage.io as io
 import torchvision.transforms as transforms
 
 
@@ -15,7 +16,7 @@ class ImageClassificationModel(nn.Module):
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(16*125*125, 120)  # for 512x512
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, num_classes)
 
@@ -23,6 +24,7 @@ class ImageClassificationModel(nn.Module):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        # print(f"Forward shape flatten: {x.shape}")
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -30,11 +32,12 @@ class ImageClassificationModel(nn.Module):
 
 
 class ImageClassificationDataset(torch.utils.data.Dataset):
-    def __init__(self, dataset_directory):
+    def __init__(self, dataset_directory, image_size):
         print(f"Loading ImageClassificationDataset from {dataset_directory}")
         self.samples = []
         self.class_names = []
         self.transform = transforms.ToTensor()
+        self.image_size = image_size
         if not os.path.isdir(dataset_directory):
             raise ValueError(f"'{dataset_directory}' is not a directory")
 
@@ -50,7 +53,7 @@ class ImageClassificationDataset(torch.utils.data.Dataset):
                 sample = os.path.join(class_directory, sample)
                 if not os.path.isfile(sample):
                     continue
-                self.samples.append((Path(sample), class_name))
+                self.samples.append((sample, class_name))
                 # print(f"Registered class '{self.samples[-1][1]}' sample '{self.samples[-1][0]}'")
 
         self.num_classes = len(self.class_names)
@@ -63,19 +66,16 @@ class ImageClassificationDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, index):
         image = io.imread(self.samples[index][0])
+        image = cv2.resize(image, (self.image_size, self.image_size))
         if self.transform:
             image = self.transform(image)
-        label = self.class_names.index(self.samples[index][1])
-        label = F.one_hot(torch.Tensor([label]).to(torch.int64), num_classes=self.num_classes)
-        # print(f"__getitem__ returning label: {label}")
+        label = torch.tensor(int(self.class_names.index(self.samples[index][1])))
         return image, label
 
 
 if __name__ == "__main__":
-    # try:
-    dataset = ImageClassificationDataset("data/DNN/objdetect/HISAS")
+    dataset = ImageClassificationDataset("data/DNN/objdetect/HISAS", image_size=512)
     model = ImageClassificationModel(dataset.num_classes)
-    options = train.TrainOptions(num_epochs=10, criterion=nn.CrossEntropyLoss(), optimizer=optim.Adam, learn_rate=5e-1, batch_size=16, test_ratio=0.2)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    options = train.TrainOptions(num_epochs=50, criterion=nn.CrossEntropyLoss(), optimizer=optim.Adam, learn_rate=0.0001, batch_size=16, test_ratio=0.2, device=device)
     train.train(model, dataset, options)
-    # except Exception as e:
-    #     print(f"Error: {e}")
--- script/dnn/train.py
+++ script/dnn/train.py
@@ -1,10 +1,12 @@
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class TrainOptions:
-    def __init__(self, num_epochs=10, learn_rate=5e-1, save_model=False, log_progress=True, plot_progress=True, criterion=nn.CrossEntropyLoss(), optimizer=optim.Adam, batch_size=16, test_ratio=0.2):
+    def __init__(self, num_epochs=10, learn_rate=5e-1, save_model=False, log_progress=True, plot_progress=True, criterion=nn.CrossEntropyLoss(), optimizer=optim.Adam, batch_size=16, test_ratio=0.2, device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')):
         self.num_epochs = num_epochs
         self.learn_rate = learn_rate
         self.save_model = save_model
@@ -14,30 +16,42 @@ class TrainOptions:
         self.optimizer = optimizer
         self.batch_size = batch_size
         self.test_ratio = test_ratio
+        self.device = device
 
 
 def train(model, dataset, options):
     test_size = int(len(dataset)*options.test_ratio)
     train_size = len(dataset)-test_size
-    print(f"Training started: train_size: {train_size}, test_size: {test_size}")
     train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=options.batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=options.batch_size, shuffle=True)
+    model.to(options.device)
     optimizer = options.optimizer(model.parameters(), lr=options.learn_rate)
 
+    print(f"Training started: train_size: {train_size}, test_size: {test_size}, batch_size: {options.batch_size}, device: {options.device}")
+    losses_train, losses_test = [], []
     for epoch in range(options.num_epochs):
-        loss_train, loss_test = train_one_epoch(model, train_loader, test_loader, optimizer, options.criterion)
+        loss_train, loss_test = train_one_epoch(model, train_loader, test_loader, optimizer, options.criterion, options.device)
+
         if options.log_progress:
-            print("Epoch {} | TrainLoss {:.2e} | TestLoss {:.2e}", epoch, loss_train, loss_test)
+            print(f"Epoch {epoch} | TrainLoss {loss_train:.2e} | TestLoss {loss_test:.2e}")
+        if options.plot_progress:
+            losses_train.append(loss_train)
+            losses_test.append(loss_test)
+            plot_train(epoch, losses_train, losses_test)
 
     print('Training finished')
     if options.save_model:
         torch.save(model.state_dict(), "model.pth")
+    if options.plot_progress:
+        plt.show()
 
 
-def train_one_epoch(model, train_loader, test_loader, optimizer, criterion):
+def train_one_epoch(model, train_loader, test_loader, optimizer, criterion, device):
     loss_train = 0.0
     for inputs, targets in train_loader:
+        inputs = inputs.to(device)
+        targets = targets.to(device)
         optimizer.zero_grad()
         outputs = model(inputs)
         loss = criterion(outputs, targets)
@@ -45,10 +59,27 @@ def train_one_epoch(model, train_loader, test_loader, optimizer, criterion):
         optimizer.step()
         loss_train += loss.item()*inputs.size(0)
 
-    loss_test = 0.0
-    for inputs, targets in test_loader:
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
-        loss_test += loss.item()*inputs.size(0)
+    with torch.no_grad():
+        loss_test = 0.0
+        for inputs, targets in test_loader:
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss_test += loss.item()*inputs.size(0)
 
     return loss_train/len(train_loader.dataset), loss_test/len(test_loader.dataset)  # sample-average loss
+
+
+def plot_train(epoch, losses_train, losses_test):
+    plt.clf()
+    plotx = np.arange(0, epoch+1, 1)
+    plt.plot(plotx, losses_train, linewidth=3, color="blue", label="train loss")
+    plt.plot(plotx, losses_test, linewidth=3, color="green", label="test loss")
+    # plt.xticks(plotx)
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.yscale("log")
+    plt.legend()
+    plt.draw()
+    plt.pause(1e-4)
