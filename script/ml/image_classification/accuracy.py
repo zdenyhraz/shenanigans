18b7b7fcf36d0beeec713e61339f1d998787aa6f
zdenyhraz <hrazdira.zdenek@gmail.com>
2023-11-15 14:15:10 +0100

    acc

--- script/ml/image_classification/accuracy.py
+++ script/ml/image_classification/accuracy.py
@@ -3,7 +3,4 @@ import torch
 
 def accuracy(pred, target):
     with torch.no_grad():
-        acc = (torch.argmax(pred, 1) == target).sum().item()/pred.size(0)
-        if acc > 1:
-            print(f"Watafak {acc}>1:\npred:\n{pred}, target:\n{target}")
-        return acc
+        return (torch.argmax(pred, 1) == target).sum().item()/pred.size(0)
