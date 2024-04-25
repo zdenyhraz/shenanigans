040ecb457e887d6bf3892ed5cb5c8b03de23d9df
zdenyhraz <hrazdira.zdenek@gmail.com>
2024-04-25 21:15:25 +0200

    tst

--- test/shenanigans/MedianBlurTest.cpp
+++ test/shenanigans/MedianBlurTest.cpp
@@ -3,7 +3,7 @@
 TEST(MedianBlur, OpenCVEquality)
 {
   const i32 medsize = 5;
-  cv::Mat mat = LoadUnitFloatImage<f32>(GetProjectDirectoryPath("test/shenanigans/baboon.png"));
+  cv::Mat mat = LoadUnitFloatImage<f32>(GetProjectDirectoryPath("test/shenanigans/data/baboon.png"));
   cv::Mat medcv;
   cv::medianBlur(mat, medcv, medsize);
   const auto med = MedianBlur<f32>(mat, medsize, medsize);
@@ -12,7 +12,7 @@ TEST(MedianBlur, OpenCVEquality)
 
 TEST(MedianBlur, OnePixelKernel)
 {
-  cv::Mat mat = LoadUnitFloatImage<f32>(GetProjectDirectoryPath("test/shenanigans/baboon.png"));
+  cv::Mat mat = LoadUnitFloatImage<f32>(GetProjectDirectoryPath("test/shenanigans/data/baboon.png"));
   const auto med = MedianBlur<f32>(mat, 1, 1);
   EXPECT_TRUE(Equal<f32>(med, mat));
 }
