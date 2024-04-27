a02f4e7a278980360c76e398d39debf8f1a8f697
zdenyhraz <hrazdira.zdenek@gmail.com>
2024-04-27 21:53:05 +0200

    idek

--- test.py
+++ test.py
@@ -10,9 +10,30 @@ def run(command, cwd=None):
         raise RuntimeError(f"Error running command {command}: {e}")
 
 
-if __name__ == '__main__':
-    cwd = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'build')
-    if not os.path.exists(cwd):
-        raise RuntimeError(f'Cannot find build directory {cwd}')
+def check_build_dir(build_dir):
+    if os.path.exists(build_dir):
+        print(f'Build directory: {build_dir}')
+    else:
+        raise RuntimeError(f'Cannot find build directory {build_dir}')
 
-    run('ctest --rerun-failed --output-on-failure', cwd)
+
+def check_test_data_dir(test_dir):
+    if os.path.exists(test_dir) and os.path.isdir(test_dir):
+        for root, dirs, files in os.walk(test_dir):
+            for file in files:
+                if file == 'baboon.png':
+                    print(f'Test data directory: {root}')
+                    return
+
+    raise RuntimeError('Cannot not find test data directory')
+
+
+if __name__ == '__main__':
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(root_dir, 'build')
+    test_dir = os.path.join(root_dir, 'test')
+
+    check_build_dir(build_dir)
+    check_test_data_dir(test_dir)
+
+    run('ctest --rerun-failed --output-on-failure', build_dir)
