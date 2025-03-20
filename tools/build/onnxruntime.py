b0bc41d0a7928424e5499a1055140ea5a7623868
zdenyhraz <hrazdira.zdenek@gmail.com>
2025-03-20 18:07:07 +0100

    onnx cache

--- tools/build/onnxruntime.py
+++ tools/build/onnxruntime.py
@@ -5,6 +5,9 @@ import platform
 import urllib.request
 from . import utils
 
+onnxruntime_install_name = "onnxruntime"
+onnxruntime_install_dir = os.path.join(utils.get_root_directory(), 'libs', onnxruntime_install_name)
+
 
 def onnxruntime_get_url():
     if utils.linux():
@@ -14,27 +17,35 @@ def onnxruntime_get_url():
     raise RuntimeError(f"Unsupported onnxruntime platform: {platform.system()}")
 
 
-def setup(build_type):
+def onnxruntime_download():
     url = onnxruntime_get_url()
     filename, extension = os.path.splitext(url.split('/')[-1])
-    install_dir = "libs/onnxruntime_test"
     archive_file = f"onnxruntime{extension}"
 
-    os.makedirs(install_dir, exist_ok=True)
+    os.makedirs(onnxruntime_install_dir, exist_ok=True)
     print(f"Downloading onnxruntime from {url}")
     urllib.request.urlretrieve(url, archive_file)
 
-    print(f"Extracting onnxruntime to {install_dir}")
+    print(f"Extracting onnxruntime to {onnxruntime_install_dir}")
     if extension == ".tgz":
         with tarfile.open(archive_file, 'r:gz') as tar_ref:
-            tar_ref.extractall(install_dir)
+            tar_ref.extractall(onnxruntime_install_dir)
     elif extension == ".zip":
         with zipfile.ZipFile(archive_file, 'r') as zip_ref:
-            zip_ref.extractall(install_dir)
+            zip_ref.extractall(onnxruntime_install_dir)
 
     print(f"Cleaning up onnxruntime archive {archive_file}")
     os.remove(archive_file)
 
-    binaries = utils.find_binaries(install_dir)
+
+def onnxruntime_installed():
+    return os.path.isdir(onnxruntime_install_dir) and any(os.scandir(onnxruntime_install_dir))
+
+
+def setup(build_type):
+    if not onnxruntime_installed():
+        onnxruntime_download()
+
+    binaries = utils.find_binaries(onnxruntime_install_dir)
     utils.copy_files_to_directory(binaries, utils.get_runtime_directory(build_type))
-    return install_dir
+    return onnxruntime_install_dir
--- tools/build/opencv.py
+++ tools/build/opencv.py
@@ -4,7 +4,7 @@ from . import utils
 
 opencv_install_name = 'opencv_install'
 opencv_install_dir = os.path.join(utils.get_root_directory(), 'libs', opencv_install_name)
-opencv_args = {
+opencv_cmake_args = {
     'CMAKE_BUILD_TYPE': 'Release',
     'OPENCV_EXTRA_MODULES_PATH': '../opencv_contrib/modules',
     'OPENCV_ENABLE_NONFREE': 'ON',
@@ -15,7 +15,7 @@ opencv_args = {
 }
 
 
-def opencv_find_root(install_dir):
+def opencv_find_root():
     candidates = []
     if os.path.exists(opencv_install_dir) and os.path.isdir(opencv_install_dir):
         for root, _, files in os.walk(opencv_install_dir):
@@ -27,20 +27,20 @@ def opencv_find_root(install_dir):
     return '' if len(candidates) == 0 else max(candidates, key=len)  # return the deepest if multiple found
 
 
-def opencv_install(jobs, opencv_install_name, opencv_install_dir):
-    print(f'Installing OpenCV: {platform.system()}/-j{jobs}/opencv_configure_args={utils.generate_configure_args(opencv_args)}')
+def opencv_install(jobs):
+    print(f'Installing OpenCV: {platform.system()}/-j{jobs}/opencv_configure_args={utils.generate_configure_args(opencv_cmake_args)}')
     cwd = os.path.join(utils.get_root_directory(), 'libs/opencv')
     os.makedirs(os.path.join(cwd, 'build'), exist_ok=True)
-    utils.run(f'cmake -B ./build {utils.generate_configure_args(opencv_args)}', cwd)
+    utils.run(f'cmake -B ./build {utils.generate_configure_args(opencv_cmake_args)}', cwd)
     utils.run(f'cmake --build ./build --config Release -j {jobs}', cwd)
     utils.run(f'{"sudo " if utils.linux() else ""}cmake --install ./build --prefix ../{opencv_install_name}', cwd)
     return opencv_find_root(opencv_install_dir)
 
 
 def setup(jobs, build_type):
-    opencv_dir = opencv_find_root(opencv_install_dir)
+    opencv_dir = opencv_find_root()
     if not opencv_dir:
-        opencv_dir = opencv_install(jobs, opencv_install_name, opencv_install_dir)
+        opencv_dir = opencv_install(jobs)
     if not opencv_dir:
         raise RuntimeError("Unable to find installed OpenCV CMake directory")
     print('OpenCV cmake directory: ', opencv_dir)
