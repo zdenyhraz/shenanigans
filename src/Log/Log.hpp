ba3f86fee97b8b6c164ee42e5200467909f48d84
zdenyhraz <hrazdira.zdenek@gmail.com>
2025-03-25 15:47:23 +0100

    devlp

--- src/Log/Log.hpp
+++ src/Log/Log.hpp
@@ -1,20 +1,16 @@
 #pragma once
 #include "Utils/Formatters.hpp"
 #include "Utils/Exception.hpp"
+#include "Logger.hpp"
 #include "LogFunction.hpp"
-#include "TerminalLogger.hpp"
 
 #ifdef GLAPI
 #  include "ImGuiLogger.hpp"
 using MainLogger = ImGuiLogger;
 #else
+#  include "TerminalLogger.hpp"
 using MainLogger = TerminalLogger;
 #endif
-
-#define SOURCE_FUNCTION std::source_location::current().function_name()
-#define SOURCE_FILE std::filesystem::relative(std::filesystem::path(std::source_location::current().file_name()), GetProjectDirectoryPath("src")).string()
-#define SOURCE_LINE std::source_location::current().line()
-#define SOURCE_LOCATION fmt::format("[{}:{}]", SOURCE_FILE, SOURCE_LINE)
 
 #define LOG_TRACE(...) MainLogger::Message(Logger::LogLevel::Trace, __VA_ARGS__)
 #define LOG_DEBUG(...) MainLogger::Message(Logger::LogLevel::Debug, __VA_ARGS__)
@@ -22,18 +18,22 @@ using MainLogger = TerminalLogger;
 #define LOG_SUCCESS(...) MainLogger::Message(Logger::LogLevel::Success, __VA_ARGS__)
 #define LOG_WARNING(...) MainLogger::Message(Logger::LogLevel::Warning, __VA_ARGS__)
 #define LOG_ERROR(...) MainLogger::Message(Logger::LogLevel::Error, __VA_ARGS__)
+
+#define EXCEPTION(...) ShenanigansException(CreateMessage(__VA_ARGS__), std::source_location::current())
 #define LOG_EXCEPTION(e) LOG_ERROR("Error: {}", e.what())
 #define LOG_UNKNOWN_EXCEPTION LOG_ERROR("Unknown error")
+
 #define LOG_PROGRESS(progress) MainLogger::SetProgress(progress)
 #define LOG_PROGRESS_NAME(progressName) MainLogger::SetProgressName(progressName)
 #define LOG_PROGRESS_RESET MainLogger::ResetProgress()
+
+#define SOURCE_FUNCTION std::source_location::current().function_name()
+#define SOURCE_FILE std::filesystem::relative(std::filesystem::path(std::source_location::current().file_name()), GetProjectDirectoryPath("src")).string()
+#define SOURCE_LINE std::source_location::current().line()
+#define SOURCE_LOCATION fmt::format("[{}:{}]", SOURCE_FILE, SOURCE_LINE)
+
 #define LOG_VARIABLE(variable) LOG_INFO("{}: {} {}", #variable, variable, SOURCE_LOCATION)
 #define LOG_FUNCTION LogFunction<MainLogger> logFunction(fmt::format("{} {}", SOURCE_FUNCTION, SOURCE_LOCATION))
 #define LOG_FUNCTION_IF(show) LogFunction<MainLogger, show> logFunction(fmt::format("{} {}", SOURCE_FUNCTION, SOURCE_LOCATION))
 #define LOG_SCOPE(funName) LogFunction<MainLogger> logScope(funName)
 #define LOG_SCOPE_IF(show, funName) LogFunction<MainLogger, show> logScope(funName)
-#ifdef PUBLIC
-#  define EXCEPTION(...) ShenanigansExceptionPublic(CreateMessage(__VA_ARGS__))
-#else
-#  define EXCEPTION(...) ShenanigansException(CreateMessage(__VA_ARGS__), std::source_location::current())
-#endif
--- src/Utils/Exception.hpp
+++ src/Utils/Exception.hpp
@@ -4,55 +4,46 @@
 #ifdef _MSC_VER
 #  include <stacktrace>
 #  define HAS_STACKTRACE 1
-#else
-#  define HAS_STACKTRACE 0
 #endif
 
 class ShenanigansException : public std::exception
 {
   std::string mMessage;
-#if HAS_STACKTRACE
+#ifdef HAS_STACKTRACE
   std::stacktrace mStacktrace;
 #endif
 
 public:
-#if HAS_STACKTRACE
+#ifdef DEVELOP
+#  ifdef HAS_STACKTRACE
   ShenanigansException(const std::string& message, const std::source_location& source, std::stacktrace&& stacktrace = std::stacktrace::current()) :
     mMessage(fmt::format("{} [{}:{}]", message, std::filesystem::relative(std::filesystem::path(source.file_name()), GetProjectDirectoryPath("src")).string(), source.line())),
     mStacktrace(std::move(stacktrace))
   {
   }
-#else
+#  else
   ShenanigansException(const std::string& message, const std::source_location& source) :
     mMessage(fmt::format("{} [{}:{}]", message, std::filesystem::relative(std::filesystem::path(source.file_name()), GetProjectDirectoryPath("src")).string(), source.line()))
   {
   }
+#  endif
+#else
+  ShenanigansException(const std::string& message, const std::source_location& source) : mMessage(message) {}
 #endif
 
   const char* what() const noexcept override { return mMessage.c_str(); }
 
   std::string Log() const
   {
-    std::string ret;
-    ret.append(mMessage);
-#if HAS_STACKTRACE
+    std::string ret = mMessage;
+#ifdef DEVELOP
+#  ifdef HAS_STACKTRACE
     ret.append("Exception stack trace:");
     size_t index = 0;
     for (const auto& entry : mStacktrace)
       ret.append(fmt::format("[{}] {} at {}:{}", index++, entry.description(), entry.source_file(), entry.source_line()));
+#  endif
 #endif
     return ret;
   }
-};
-
-class ShenanigansExceptionPublic : public std::exception
-{
-  std::string mMessage;
-
-public:
-  ShenanigansExceptionPublic(const std::string& message) : mMessage(message) {}
-
-  const char* what() const noexcept override { return mMessage.c_str(); }
-
-  std::string Log() const { return mMessage; }
 };
