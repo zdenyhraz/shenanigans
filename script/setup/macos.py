4f532764c171dd2ffe0780b7c5a811cd0d67d7a8
zdenyhraz <hrazdira.zdenek@gmail.com>
2025-04-10 16:32:02 +0200

    ++

--- script/setup/macos.py
+++ script/setup/macos.py
@@ -25,9 +25,10 @@ def opengl_install():
 def gcc_install(version='14'):
     utils.run('brew search gcc')
     utils.run(f'brew install gcc@{version}')
-    utils.run(f'brew unlink gcc@{version}')
+    utils.run('brew unlink gcc')
     utils.run(f'brew link --overwrite --force gcc@{version}')
     utils.run(f'gcc-{version} --version')
+    utils.run(f'g++-{version} --version')
     os.environ['CC'] = f'gcc-{version}'
     os.environ['CXX'] = f'g++-{version}'
 
@@ -35,9 +36,10 @@ def gcc_install(version='14'):
 def clang_install(version='18'):
     utils.run('brew search clang')
     utils.run(f'brew install llvm@{version}')
-    utils.run(f'brew unlink llvm@{version}')
+    utils.run('brew unlink llvm')
     utils.run(f'brew link --overwrite --force llvm@{version}')
     utils.run(f'clang-{version} --version')
+    utils.run(f'clang++-{version} --version')
     os.environ['CC'] = f'clang-{version}'
     os.environ['CXX'] = f'clang++-{version}'
 
