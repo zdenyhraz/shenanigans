57dabfeb7c5151f1978d0b2c838be208fcfef8b7
zdenyhraz <hrazdira.zdenek@gmail.com>
2025-04-10 22:41:38 +0200

    idk

--- script/setup/opencv.py
+++ script/setup/opencv.py
@@ -6,89 +6,42 @@ from script.log import log
 opencv_install_name = 'opencv_install'
 opencv_install_dir = os.path.join(utils.get_root_directory(), 'libs', opencv_install_name)
 opencv_cmake_args = {
-    'CMAKE_BUILD_TYPE': 'Release',
+    # build configuration
+    'CMAKE_BUILD_TYPE': 'RELEASE',
+    'BUILD_SHARED_LIBS': 'OFF',
+
+    # extra modules
     'OPENCV_EXTRA_MODULES_PATH': '../opencv_contrib/modules',
     'OPENCV_ENABLE_NONFREE': 'ON',
-    'OPENCV_PYTHON_SKIP': 'ON',
-    'OPENCV_ENABLE_ALLOCATOR_STATS': 'OFF',
-    'OPENCV_ENABLE_MEMORY_SANITIZER': 'OFF',
-    'OPENCV_ENABLE_PROFILING': 'OFF',
 
-    # essentials
-    'BUILD_SHARED_LIBS': 'OFF',
-    'BUILD_OPENCV_CORE': 'ON',
-    'BUILD_OPENCV_IMGPROC': 'ON',
-    'BUILD_OPENCV_IMGCODECS': 'ON',
-    'BUILD_OPENCV_HIGHGUI': 'ON',
-    'BUILD_OPENCV_VIDEOIO': 'ON',
-
-    # bindings and extras
-    'BUILD_OPENCV_PYTHON': 'OFF',
-    'BUILD_OPENCV_JAVA': 'OFF',
-    'BUILD_OPENCV_APPS': 'OFF',
-    'BUILD_EXAMPLES': 'OFF',
+    # tests and docs
     'BUILD_TESTS': 'OFF',
     'BUILD_PERF_TESTS': 'OFF',
     'BUILD_DOCS': 'OFF',
-    'BUILD_PACKAGE': 'OFF',
-    'BUILD_OPENCV_JAVA_BINDINGS_GEN': 'OFF',
-    'BUILD_OPENCV_PYTHON_BINDINGS_GEN': 'OFF',
-    'BUILD_OPENCV_JS': 'OFF',
-    'BUILD_OPENCV_TS': 'OFF',
-    'BUILD_OPENCV_WORLD': 'OFF',
-    'BUILD_OPENCV_FREETYPE': 'OFF',
-    'BUILD_OPENCV_GAPI': 'OFF',
-    'BUILD_OPENCV_HDF': 'OFF',
-    'BUILD_OPENCV_SFM': 'OFF',
-    'BUILD_OPENCV_ARUCO': 'OFF',
-    'BUILD_OPENCV_BIOINSPIRED': 'OFF',
-    'BUILD_OPENCV_DATASETS': 'OFF',
-    'BUILD_OPENCV_LINE_DESCRIPTOR': 'OFF',
-    'BUILD_OPENCV_QUALITY': 'OFF',
-    'BUILD_OPENCV_REG': 'OFF',
-    'BUILD_OPENCV_RGBD': 'OFF',
-    'BUILD_OPENCV_SALIENCY': 'OFF',
-    'BUILD_OPENCV_STRUCTURED_LIGHT': 'OFF',
-    'BUILD_OPENCV_TEXT': 'OFF',
-    'BUILD_OPENCV_TRACKING': 'OFF',
-    'BUILD_OPENCV_VIDEOSTAB': 'OFF',
+    'BUILD_EXAMPLES': 'OFF',
 
-    # unwanted modules
+    # bindings and apps
+    'BUILD_OPENCV_APPS': 'OFF',
+    'BUILD_OPENCV_JAVA': 'OFF',
+    'BUILD_OPENCV_PYTHON': 'OFF',
+    'OPENCV_PYTHON_SKIP': 'ON',
+
+    # modules
+    'BUILD_OPENCV_WECHAT_QRCODE': 'OFF',
+    'BUILD_OPENCV_CALIB3D': 'OFF',
     'BUILD_OPENCV_DNN': 'OFF',
     'BUILD_OPENCV_ML': 'OFF',
-    'BUILD_OPENCV_OBJDETECT': 'OFF',
     'BUILD_OPENCV_PHOTO': 'OFF',
-    'BUILD_OPENCV_VIDEO': 'OFF',
     'BUILD_OPENCV_STITCHING': 'OFF',
-    'BUILD_OPENCV_CALIB3D': 'OFF',
-    'BUILD_OPENCV_SHAPE': 'OFF',
-    'BUILD_OPENCV_SUPERRES': 'OFF',
-    'BUILD_OPENCV_FLANN': 'OFF',
-    'BUILD_OPENCV_TS': 'OFF',
-    'BUILD_OPENCV_CONTRIB': 'OFF',
-    'BUILD_OPENCV_WECHAT_QRCODE': 'OFF',
+    'BUILD_OPENCV_VIDEO': 'OFF',
+    'BUILD_OPENCV_VIDEOSTAB': 'OFF',
+    'BUILD_OPENCV_WORLD': 'OFF',
+    'BUILD_OPENCV_XFEATURES2D': 'ON',
+    'BUILD_OPENCV_LINE_DESCRIPTOR': 'OFF',
+    'BUILD_OPENCV_SALIENCY': 'OFF',
 
-    # hardware acceleration
+    # optional dependencies
     'WITH_CUDA': 'OFF',
-    'WITH_OPENCL': 'OFF',
-    'WITH_OPENGL': 'OFF',
-    'WITH_V4L': 'OFF',
-    'WITH_FFMPEG': 'OFF',
-    'WITH_GSTREAMER': 'OFF',
-    'WITH_TBB': 'OFF',
-    'WITH_PTHREADS_PF': 'OFF',
-    'WITH_EIGEN': 'OFF',
-    'WITH_IPP': 'OFF',
-    'WITH_1394': 'OFF',
-    'WITH_QT': 'OFF',
-    'WITH_JASPER': 'OFF',
-    'WITH_OPENEXR': 'OFF',
-
-    # image formats
-    'WITH_PNG': 'ON',
-    'WITH_JPEG': 'ON',
-    'WITH_TIFF': 'ON',
-    'WITH_WEBP': 'OFF',
 }
 
 
