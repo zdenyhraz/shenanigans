33119995f3e519c24a39dfa30be981c64138a42d
zdenyhraz <hrazdira.zdenek@gmail.com>
2025-04-11 12:46:02 +0200

    environ -version

--- script/setup/macos.py
+++ script/setup/macos.py
@@ -27,9 +27,9 @@ def gcc_install(version='14'):
     utils.run(f'brew install gcc@{version}')
     utils.run('brew unlink gcc')
     utils.run(f'brew link --overwrite --force gcc@{version}')
-    utils.run(f'gcc-{version} --version')
-    os.environ['CC'] = f'gcc-{version}'
-    os.environ['CXX'] = f'g++-{version}'
+    utils.run('gcc --version')
+    os.environ['CC'] = 'gcc'
+    os.environ['CXX'] = 'g++'
 
 
 def clang_install(version='18'):
@@ -37,9 +37,9 @@ def clang_install(version='18'):
     utils.run(f'brew install llvm@{version}')
     utils.run('brew unlink llvm')
     utils.run(f'brew link --overwrite --force llvm@{version}')
-    utils.run(f'clang-{version} --version')
-    os.environ['CC'] = f'clang-{version}'
-    os.environ['CXX'] = f'clang++-{version}'
+    utils.run('clang --version')
+    os.environ['CC'] = 'clang'
+    os.environ['CXX'] = 'clang++'
 
 
 def compiler_install(compiler):
