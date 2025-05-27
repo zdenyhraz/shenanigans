7ea43d3c9b435064324225a07b7dcafc20dfa26d
zdenyhraz <hrazdira.zdenek@gmail.com>
2025-05-27 20:23:43 +0200

    -python step

---
+++ script/ci/clang-tidy.py
@@ -0,0 +1,8 @@
+from script.setup import utils
+
+if __name__ == '__main__':
+    try:
+        utils.run('apt-get install clang-tidy')
+        utils.run('find ../src -type f \( -name "*.cpp" -o -name "*.hpp" \) | xargs clang-tidy -p=.', 'build')
+    except Exception as e:
+        raise
