f7f2fdb9367200678d55b7918b284d2943e8bcf2
zdenyhraz <hrazdira.zdenek@gmail.com>
2025-05-27 20:34:33 +0200

    raw

--- script/ci/cpplint.py
+++ script/ci/cpplint.py
@@ -3,6 +3,6 @@ from script.setup import utils
 if __name__ == '__main__':
     try:
         utils.run('pip install cpplint')
-        utils.run('find apps/ src/ -type f \( -iname \*.hpp -o -iname \*.cpp \) | xargs cpplint')
+        utils.run(r'find apps/ src/ -type f \( -iname \*.hpp -o -iname \*.cpp \) | xargs cpplint')
     except Exception as e:
         raise
