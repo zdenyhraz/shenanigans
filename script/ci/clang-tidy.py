482b8912dcd11afc291e9f200900ceab8b69ef31
zdenyhraz <hrazdira.zdenek@gmail.com>
2025-05-27 20:42:17 +0200

    find /

--- script/ci/clang-tidy.py
+++ script/ci/clang-tidy.py
@@ -3,6 +3,6 @@ from script.setup import utils
 if __name__ == '__main__':
     try:
         utils.run('apt-get install clang-tidy')
-        utils.run(r'find ../src -type f \( -name "*.cpp" -o -name "*.hpp" \) | xargs clang-tidy -p=.', 'build')
+        utils.run("find src/ apps/ -name '*.cpp' -o -name '*.h' | xargs clang-tidy -p=.", 'build')
     except Exception as e:
         raise
--- script/ci/cpplint.py
+++ script/ci/cpplint.py
@@ -3,6 +3,6 @@ from script.setup import utils
 if __name__ == '__main__':
     try:
         utils.run('pip install cpplint')
-        utils.run(r'find apps/ src/ -type f \( -iname \*.hpp -o -iname \*.cpp \) | xargs cpplint')
+        utils.run("find src/ apps/ -name '*.cpp' -o -name '*.h' | xargs cpplint")
     except Exception as e:
         raise
