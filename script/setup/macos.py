20882544411752bbb528a353521beca65e4ec9fd
zdenyhraz <hrazdira.zdenek@gmail.com>
2025-05-28 01:21:20 +0200

    clang apple

--- script/setup/macos.py
+++ script/setup/macos.py
@@ -46,7 +46,7 @@ def compiler_install(compiler):
     if compiler == 'gcc':
         return gcc_install()
     if compiler == 'clang':
-        return  # use native apple clang
+        return clang_install()
     raise RuntimeError(f'Unsupported compiler {compiler}')
 
 
