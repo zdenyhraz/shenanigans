635cb1698fee985714be1f265b82563c53a585db
zdenyhraz <hrazdira.zdenek@gmail.com>
2025-05-28 14:54:17 +0200

    --pip install --upgrade pip

--- script/setup/python.py
+++ script/setup/python.py
@@ -4,5 +4,4 @@ from . import utils
 def setup(args):
     requirements_file = "requirements.txt"
     utils.run('python --version')
-    utils.run('pip install --upgrade pip')
     utils.run(f'pip install -r {requirements_file}')
